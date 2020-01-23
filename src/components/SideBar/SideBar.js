a9cab3a46c4a1ec60de951fa89785415275733cb
Mariana Furyk <mariana.furyk@globallogic.com>
2020-01-23 14:52:59 +0200

    Refactored code, added sidebar navigation icons

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SideBarView from './SideBarView'
 
 const SideBar = () => {
-  return <SideBarView />
+  const [currentPage, setCurrentPage] = useState('jobs')
+
+  const handleMenuClick = value => {
+    document
+      .getElementsByClassName('active sidebar__menu__item')[0]
+      .classList.remove('active')
+    setCurrentPage(value)
+  }
+  return <SideBarView currentPage={currentPage} onClick={handleMenuClick} />
 }
 
 export default SideBar
