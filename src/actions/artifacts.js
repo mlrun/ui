798818903cb7e0c80822102ce108e747e692673f
IvanVoznyi <60106351+IvanVoznyi@users.noreply.github.com>
2020-02-05 13:26:15 +0200

    Impl [Artifacts] Visualization of a tree of artifacts (#13)

--- src/actions/artifacts.js
+++ src/actions/artifacts.js
@@ -1,16 +1,31 @@
 import artifactsApi from '../api/artifacts-api'
 import { parseKeyValues } from '../utils'
+import _ from 'lodash'
 
 // import artifactsMock from '../artifacts.json'
 const artifactsAction = {
   fetchArtifacts: () => dispatch => {
     return artifactsApi.getArtifacts().then(({ data }) => {
-      // data.artifacts = artifactsMock.artifacts
-      const newArtifacts = data.artifacts.map(item => {
-        return Object.assign(item, {
-          labels: parseKeyValues(item.labels)
+      let groupArtifacts = _(data.artifacts)
+        .groupBy('key')
+        .mapValues(item => _.groupBy(item, 'tree'))
+        .value()
+
+      let newArtifacts = []
+
+      for (const key in groupArtifacts) {
+        let newTree = []
+        for (const _key in groupArtifacts[key]) {
+          let group = groupArtifacts[key][_key].map(item => {
+            return Object.assign(item, { labels: parseKeyValues(item.labels) })
+          })
+          newTree.push(group)
+        }
+        newArtifacts.push({
+          key: key,
+          tree: newTree
         })
-      })
+      }
       dispatch(artifactsAction.setArtifacts({ artifacts: newArtifacts }))
       return newArtifacts
     })
