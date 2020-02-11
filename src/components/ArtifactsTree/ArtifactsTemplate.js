b6fc70efabe35234a54cc88b4f733e376ec17664
Ivan Voznyi <ivan.voznyi@globallogic.com>
2020-02-11 11:00:33 +0200

    fix some bugs

---
+++ src/components/ArtifactsTree/ArtifactsTemplate.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import Tooltip from '../ArtifactsTooltip/Tooltip'
+import { truncateUid, formatDatetime } from '../../utils'
+import Download from '../../common/Download/Download'
+
+const templateArifacts = item => {
+  return (
+    <>
+      <div className="column_path">
+        <div className="path_container">
+          <div className="path_container_item">
+            <div
+              className="path_container_value"
+              title={item.target_path.length > 60 ? item.target_path : null}
+            >
+              {item.target_path}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="column_type">
+        <div className="type_container">
+          <div className="type_container_item">{item.kind}</div>
+        </div>
+      </div>
+      <div className="column_labels">
+        <div className="labels_container">
+          {item.labels.map((label, index) => {
+            return (
+              <div key={index} className="labels_container_item" title={label}>
+                <span className="label">{label}</span>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      <div className="column_producer">
+        <div className="producer_container">
+          <div className="producer_container_item">
+            {item.producer && (
+              <Tooltip
+                kind={item.producer.kind}
+                owner={item.producer.owner}
+                to={`/jobs/${item.producer.uri}/info`}
+                name={item.producer.name}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="column_hash">
+        <div className="hash_container" title={item.hash}>
+          <div className="hash_container_item">{truncateUid(item.hash)}</div>
+        </div>
+      </div>
+      <div className="column_started_at">
+        <div className="started_at_container">
+          <div className="started_at_container_item">
+            {formatDatetime(new Date(item.updated))}
+          </div>
+        </div>
+      </div>
+      <div className="column_download">
+        <Download path={item.target_path} />
+      </div>
+    </>
+  )
+}
+
+export default templateArifacts
