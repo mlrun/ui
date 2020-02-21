3458caa6c16ffe763b89d2b170ae215c44fe014c
Ivan Voznyi <ivan.voznyi@globallogic.com>
2020-02-21 18:44:39 +0200

    added yaml highlight syntax and tree filter component

--- src/components/ProjectsPage/Projects.js
+++ src/components/ProjectsPage/Projects.js
@@ -1,11 +1,9 @@
 import React, { useEffect } from 'react'
 import projectsAction from '../../actions/projects'
-import Breadcrums from '../../common/Breadcrumbs/Breadcrumbs'
+import Breadcrumbs from '../../common/Breadcrumbs/Breadcrumbs'
 import Loader from '../../common/Loader/Loader'
-import { Link } from 'react-router-dom'
+import ProjectCard from '../../elements/ProjectCard/ProjectCard'
 import { connect } from 'react-redux'
-import artifactIcon from '../../images/file-chart.png'
-import jobsIcon from '../../images/check-all.png'
 import './projects.scss'
 const Projects = ({ projects, fetchProjects, match }) => {
   useEffect(() => {
@@ -14,26 +12,12 @@ const Projects = ({ projects, fetchProjects, match }) => {
   return (
     <>
       <div className="breadcrums_container">
-        <Breadcrums match={match}></Breadcrums>
+        <Breadcrumbs match={match} />
       </div>
       <div className="projects_wrapper">
         {projects.length !== 0 ? (
           projects.map(project => {
-            return (
-              <div key={project} className="project_card">
-                <div className="project_card_header">{project}</div>
-                <div className="project_card_links">
-                  <Link to={`/projects/${project}/jobs`}>
-                    <img src={jobsIcon} alt="jobs" />
-                    <div>Jobs</div>
-                  </Link>
-                  <Link to={`/projects/${project}/artifacts`}>
-                    <img src={artifactIcon} alt="artifact" />
-                    <div>Artifacts</div>
-                  </Link>
-                </div>
-              </div>
-            )
+            return <ProjectCard key={project} project={project} />
           })
         ) : (
           <Loader />
