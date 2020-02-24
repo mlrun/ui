e31a0600524bbefbd7c325a29f98bfc940f3b5e5
Ivan Voznyi <ivan.voznyi@globallogic.com>
2020-02-24 17:59:27 +0200

    fixed conflict

--- src/components/ArtifacstFilterMenu/ArtifactsFilterMenu.js
+++ src/components/ArtifacstFilterMenu/ArtifactsFilterMenu.js
@@ -7,13 +7,8 @@ import ArtifactFilterTree from '../ArtifactsFilterTree/ArtifactsFilterTree'
 const ArtifactsFilterMenu = ({ filters }) => {
   const [items] = useState(['Latest'])
 
-<<<<<<< HEAD
   const handleChange = item => {
     // console.log(item)
-=======
-  const handleChnage = item => {
-    console.log(item)
->>>>>>> fdefd433b03155f1aeac596ea016c94089e07a9d
   }
 
   return (
@@ -24,11 +19,7 @@ const ArtifactsFilterMenu = ({ filters }) => {
             key={filter}
             value="Latest"
             items={items}
-<<<<<<< HEAD
             onChange={handleChange}
-=======
-            onChange={handleChnage}
->>>>>>> fdefd433b03155f1aeac596ea016c94089e07a9d
           />
         ) : (
           <Select filter={filter} key={filter} />
