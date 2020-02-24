6d01b9f2967edf79b7db423a4aec78d16fbd9004
Eran Nussbaum <eran-nussbaum@users.noreply.github.com>
2020-02-24 20:17:49 +0200

    Impl [Artifacts][Jobs] Show path as-is from `target_path`

--- src/components/DetailsArtifacts/DetailsArtifactsView.js
+++ src/components/DetailsArtifacts/DetailsArtifactsView.js
@@ -22,7 +22,9 @@ const DetailsArtifactsView = ({ items, handleClick }) => (
               {item.key}
             </div>
             <div className="table__item_artifacts__row_item table__item_artifacts__row_item_long">
-              {item.target_path.schema + item.target_path.path}
+              {`${
+                item.target_path.schema ? `${item.target_path.schema}://` : ''
+              }${item.target_path.path}`}
             </div>
             <div className="table__item_artifacts__row_item">
               size: {item.size}
