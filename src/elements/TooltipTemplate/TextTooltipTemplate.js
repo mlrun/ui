4751671ca6be2046e449b4b1fd94d15b627c0d66
Ivan Voznyi <ivan.voznyi@globallogic.com>
2020-02-28 17:12:28 +0200

    added tooltip component

---
+++ src/elements/TooltipTemplate/TextTooltipTemplate.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './texttooltiptemplate.scss'
+const TextTooltipTemplate = ({ text }) => {
+  return <div className="text_tooltip_container">{text}</div>
+}
+export default TextTooltipTemplate
