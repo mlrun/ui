538f3fab95fcc4f3b4edf9d1b06d88094f6b3b81
Mariana Furyk <mariana.furyk@globallogic.com>
2020-03-25 15:59:07 +0200

    Refactored wrong filename

--- src/elements/CreateJobCardTemplate/CreateJobCardTemplate.js
+++ src/elements/CreateJobCardTemplate/CreateJobCardTemplate.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import './createJobCatdTemplate.scss'
+import './createJobCardTemplate.scss'
 
 const CreateJobCardTemplate = ({ name, status }) => {
   return (
