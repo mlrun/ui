7074377a87d42a79146942b26918e2bc24abc230
Eran Nussbaum <eran-nussbaum@users.noreply.github.com>
2020-05-19 20:54:01 +0300

    Impl [Config] Add `x-v3io-session-key` req header in dev proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,9 +5,16 @@ module.exports = function(app) {
     '/api',
     createProxyMiddleware({
       target: 'http://18.221.106.241:30080',
-      changeOrigin: true
+      changeOrigin: true,
+      onProxyReq: function(proxyReq, req, res) {
+        proxyReq.setHeader(
+          'x-v3io-session-key',
+          '0a55b9d4-431e-443a-848b-c20f727c9943'
+        )
+      }
     })
   )
+
   app.use(
     '/function-catalog',
     createProxyMiddleware({
