af8b24d9ac0c3f21dc91e4c0beb92ca3a3f073fb
mariana-furyk <58301139+mariana-furyk@users.noreply.github.com>
2020-07-01 14:36:14 +0300

    Fix [Jobs] Infinite loop when no workflows returned (#154)

--- src/utils/createJobsContent.js
+++ src/utils/createJobsContent.js
@@ -3,66 +3,70 @@ import measureTime from './measureTime'
 
 const createJobsContent = (content, groupedByWorkflow) => {
   return content.map(contentItem => {
-    let type = contentItem.labels?.find(label => label.includes('kind:'))
-    type = type?.slice(type.indexOf(':') + 2)
+    if (contentItem) {
+      let type = contentItem.labels?.find(label => label.includes('kind:'))
+      type = type?.slice(type.indexOf(':') + 2)
 
-    return {
-      name: {
-        value: contentItem.name,
-        class: 'jobs_medium'
-      },
-      type: {
-        value: groupedByWorkflow ? 'workflow' : type,
-        class: 'jobs_extra-small',
-        type: 'type'
-      },
-      uid: {
-        value: contentItem.uid || contentItem.id,
-        class: 'jobs_small',
-        type: 'hidden'
-      },
-      startTime: {
-        value: formatDatetime(
-          contentItem.startTime || new Date(contentItem.created_at)
-        ),
-        class: 'jobs_small',
-        type: 'date'
-      },
-      duration: {
-        value: measureTime(
-          contentItem.startTime || new Date(contentItem.created_at),
-          (contentItem.state !== 'running' && contentItem.updated) ||
-            (contentItem.status !== 'error' &&
-              new Date(contentItem.finished_at))
-        ),
-        class: 'jobs_extra-small',
-        type: 'duration'
-      },
-      owner: {
-        value: contentItem.owner,
-        class: 'jobs_extra-small'
-      },
-      labels: {
-        value: contentItem.labels,
-        class: 'jobs_extra-small',
-        type: 'labels'
-      },
-      parameters: {
-        value: contentItem.parameters,
-        class: 'jobs_extra-small',
-        type: 'parameters'
-      },
-      resultsChips: {
-        value: contentItem.resultsChips,
-        class: 'jobs_big',
-        type: 'results'
-      },
-      updated: {
-        value: contentItem.updated || new Date(contentItem.finished_at),
-        class: 'jobs_small',
-        type: 'hidden'
+      return {
+        name: {
+          value: contentItem.name,
+          class: 'jobs_medium'
+        },
+        type: {
+          value: groupedByWorkflow ? 'workflow' : type,
+          class: 'jobs_extra-small',
+          type: 'type'
+        },
+        uid: {
+          value: contentItem.uid || contentItem?.id,
+          class: 'jobs_small',
+          type: 'hidden'
+        },
+        startTime: {
+          value: formatDatetime(
+            contentItem.startTime || new Date(contentItem.created_at)
+          ),
+          class: 'jobs_small',
+          type: 'date'
+        },
+        duration: {
+          value: measureTime(
+            contentItem.startTime || new Date(contentItem.created_at),
+            (contentItem.state !== 'running' && contentItem.updated) ||
+              (contentItem.status !== 'error' &&
+                new Date(contentItem.finished_at))
+          ),
+          class: 'jobs_extra-small',
+          type: 'duration'
+        },
+        owner: {
+          value: contentItem.owner,
+          class: 'jobs_extra-small'
+        },
+        labels: {
+          value: contentItem.labels,
+          class: 'jobs_extra-small',
+          type: 'labels'
+        },
+        parameters: {
+          value: contentItem.parameters,
+          class: 'jobs_extra-small',
+          type: 'parameters'
+        },
+        resultsChips: {
+          value: contentItem.resultsChips,
+          class: 'jobs_big',
+          type: 'results'
+        },
+        updated: {
+          value: contentItem.updated || new Date(contentItem.finished_at),
+          class: 'jobs_small',
+          type: 'hidden'
+        }
       }
     }
+
+    return []
   })
 }
 
