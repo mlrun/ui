48390ed95ef7cb5f7a86681b3ca7444a9ddc3009
Mariana Furyk <mariana.furyk@globallogic.com>
2020-07-22 18:51:18 +0300

    refactored code

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import logo from '../../images/iguazio-corner-logo.png'
+import logo from '../../images/iguazio-logo.png'
 
 import './header.scss'
 
