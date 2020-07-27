fff7548af20d18257c3d5138c1c5cd560ff76ba7
mariana-furyk <58301139+mariana-furyk@users.noreply.github.com>
2020-07-27 16:51:31 +0300

    Fix [Jobs] Wizard: missing handler in request to create a job (#186)

--- src/components/JobsPanel/JobsPanel.js
+++ src/components/JobsPanel/JobsPanel.js
@@ -203,7 +203,8 @@ const JobsPanel = ({
           ...jobsStore.newJob.task.spec,
           output_path: panelState.outputPath,
           input_path: panelState.inputPath,
-          function: `${match.params.projectName}/${selectedFunction.metadata.name}@${selectedFunction.metadata.hash}`
+          function: `${match.params.projectName}/${selectedFunction.metadata.name}@${selectedFunction.metadata.hash}`,
+          handler: panelState.currentFunctionInfo.method
         }
       }
     }
