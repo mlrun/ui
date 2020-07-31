9046562011508673bbbb7dd2b44a71ea63f1ef16
IvanVoznyi <ivan.voznyi@globallogic.com>
2020-07-31 17:04:35 +0300

    fixed bug if a path on the artifact is undefined

--- src/utils/parseTargetPath.js
+++ src/utils/parseTargetPath.js
@@ -1,4 +1,10 @@
 export default targetPath => {
+  if (!targetPath) {
+    return {
+      schema: '',
+      path: ''
+    }
+  }
   const index = targetPath.indexOf('://')
   const target_path = {
     schema: targetPath.includes('://') ? targetPath.slice(0, index) : '',
