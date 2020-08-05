209cd8e7fbbdc7068100305ecda467ac8c246cf7
Mariana Furyk <mariana.furyk@globallogic.com>
2020-08-05 11:26:58 +0300

    [General] Removed left nav bar

--- src/layout/Page/PageView.js
+++ src/layout/Page/PageView.js
@@ -1,19 +1,11 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Header from '../Header/Header'
-import SideBar from '../SideBar/SideBar'
 
-export default function PageView({ children, location }) {
+export default function PageView({ children }) {
   return (
     <div className="App">
       <Header />
-      <SideBar location={location} />
       <main>{children}</main>
     </div>
   )
-}
-
-PageView.propTypes = {
-  children: PropTypes.element.isRequired,
-  location: PropTypes.shape({}).isRequired
 }
