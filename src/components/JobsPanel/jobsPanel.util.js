e888f6266dba3965d6b28d000a28e10b8b3f8fde
mariana-furyk <58301139+mariana-furyk@users.noreply.github.com>
2020-08-31 13:50:08 +0300

    Fix [Create Job] Environment variables: wrong backend model (#220)

--- src/components/JobsPanel/jobsPanel.util.js
+++ src/components/JobsPanel/jobsPanel.util.js
@@ -158,7 +158,7 @@ export const generateTableData = (
         ? volumeMounts.map(volumeMounts => volumeMounts.data)
         : [],
       volumes,
-      environmentVariables: {},
+      environmentVariables: [],
       secret_sources: []
     })
   }
