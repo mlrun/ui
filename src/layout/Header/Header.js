29f9a5ff101f46e518ac2649ae01f2f64d4f29e6
Eran Nussbaum <eran-nussbaum@users.noreply.github.com>
2020-09-07 16:10:10 +0300

    Impl [Header] Change Iguazio logo to MLRun logo; Update favicon (#231)

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import logo from '../../images/iguazio-logo.png'
+import logo from '../../images/mlrun-logo-circle-small.png'
 
 import './header.scss'
 
