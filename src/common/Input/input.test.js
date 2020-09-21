ff915467424ae4101ca22389ccfe1488837681cb
IvanVoznyi <ivan.voznyi@globallogic.com>
2020-09-21 13:01:20 +0300

    fixed error "jest" when running tests

--- src/common/Input/input.test.js
+++ src/common/Input/input.test.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { render, fireEvent, cleanup, screen } from '@testing-library/react'
-
+import { render, fireEvent, cleanup } from '@testing-library/react'
 import Input from './Input'
 
 jest.mock('../../images/warning.svg', () => ({
