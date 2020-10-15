5edf5cd56a59b7b879602082fe7546ef5450b1fb
Eran Nussbaum <eran-nussbaum@users.noreply.github.com>
2020-10-15 14:32:18 +0300

    Impl [General] On production deploy on /mlrun path
    Also optionally hide the header with `mlrunUi.headerHidden` in
    `localStorage` set to `'true'`.

--- src/layout/Page/PageView.js
+++ src/layout/Page/PageView.js
@@ -2,9 +2,11 @@ import React from 'react'
 import Header from '../Header/Header'
 
 export default function PageView({ children }) {
+  const headerShown =
+    window.localStorage.getItem('mlrunUi.headerHidden') !== 'true'
   return (
     <div className="App">
-      <Header />
+      {headerShown && <Header />}
       <main>{children}</main>
     </div>
   )
