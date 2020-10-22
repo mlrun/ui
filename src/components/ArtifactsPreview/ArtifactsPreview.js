c7dbd8dbb45e5fa7dbacd95363ea5da8b8a1063e
Mariana Furyk <mariana.furyk@globallogic.com>
2020-10-22 18:50:27 +0300

    [General] Changed loading error message

--- src/components/ArtifactsPreview/ArtifactsPreview.js
+++ src/components/ArtifactsPreview/ArtifactsPreview.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import PropTypes from 'prop-types'
+import Prism from 'prismjs'
 
 import api from '../../api/artifacts-api'
 
@@ -11,8 +12,12 @@ import { createArtifactPreviewContent } from '../../utils/createArtifactPreviewC
 import './artifactaPreview.scss'
 
 const ArtifactsPreview = ({ artifact }) => {
-  const [isError, setIsError] = useState(false)
+  const [error, setError] = useState({
+    text: '',
+    body: ''
+  })
   const [preview, setPreview] = useState([])
+  const [showError, setShowError] = useState(false)
 
   const getArtifactPreview = useCallback((schema, path, user) => {
     return api.getArtifactPreview(schema, path, user).then(res => {
@@ -22,7 +27,11 @@ const ArtifactsPreview = ({ artifact }) => {
 
   useEffect(() => {
     if (artifact.schema && !artifact.extra_data) {
-      setIsError(false)
+      setError({
+        text: '',
+        body: ''
+      })
+      setShowError(false)
       setPreview([
         {
           type: 'table',
@@ -44,10 +53,17 @@ const ArtifactsPreview = ({ artifact }) => {
               ...prevState,
               { ...content, header: previewItem.header }
             ])
-            setIsError(false)
+            setError({
+              text: '',
+              body: ''
+            })
+            setShowError(false)
           })
-          .catch(() => {
-            setIsError(true)
+          .catch(err => {
+            setError({
+              text: `${err.response.status} ${err.response.data}`,
+              body: JSON.stringify(err.response)
+            })
           })
       })
     } else {
@@ -58,18 +74,40 @@ const ArtifactsPreview = ({ artifact }) => {
       )
         .then(content => {
           setPreview([content])
-          setIsError(false)
+          setError({
+            text: '',
+            body: ''
+          })
+          setShowError(false)
         })
-        .catch(() => {
-          setIsError(true)
+        .catch(err => {
+          setError({
+            text: `${err.response.status} ${err.response.data}`,
+            body: JSON.stringify(err.response)
+          })
         })
     }
   }, [artifact, getArtifactPreview])
 
-  return isError ? (
+  return error.text.length > 0 ? (
     <div className="error_container">
-      <h1>Sorry, something went wrong.</h1>
-      <h3>We're working on it and we'll get it fixed as soon as we can.</h3>
+      <h1>Failed with HTTP error:</h1>
+      <h3>{error.text}</h3>
+      <button
+        className="error-details btn"
+        onClick={() => setShowError(state => !state)}
+      >
+        View details
+      </button>
+      {showError && (
+        <pre className="json-content">
+          <code
+            dangerouslySetInnerHTML={{
+              __html: Prism.highlight(error.body, Prism.languages.json, 'json')
+            }}
+          />
+        </pre>
+      )}
     </div>
   ) : preview.length === 0 ? (
     <div className="loader-container">
