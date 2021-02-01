c91366a029444bb6af7a26d3fb8f4cd1212f3886
Eran Nussbaum <eran-nussbaum@users.noreply.github.com>
2021-02-01 19:05:29 +0200

    Fix [Create Job] Crashes when adding a new label (Firefox only) (#378)

--- src/components/ChipForm/ChipForm.js
+++ src/components/ChipForm/ChipForm.js
@@ -82,8 +82,9 @@ const ChipForm = ({
   const outsideClick = useCallback(
     event => {
       event.stopPropagation()
+      const elementPath = event.path ?? event.composedPath?.()
 
-      if (!event.path.includes(refInputContainer.current)) {
+      if (!elementPath.includes(refInputContainer.current)) {
         onChange(chip, 'Click')
       }
     },
