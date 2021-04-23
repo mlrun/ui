5417a4a0a3378b913404fb2e6da3d8ebb7d2a3e6
illia prokopchuk <illia.prokopchuk@globallogic.com>
2021-04-23 13:10:53 +0300

    [Feature Vectors] Requested features: remove and edit alias

--- src/components/DetailsRequestedFeatures/detailsRequestedFeatures.utils.js
+++ src/components/DetailsRequestedFeatures/detailsRequestedFeatures.utils.js
@@ -2,5 +2,6 @@ export const headers = [
   { label: 'Project name', id: 'project-name' },
   { label: 'Feature set', id: 'feature-set' },
   { label: 'Feature', id: 'feature' },
-  { label: 'Alias', id: 'alias' }
+  { label: 'Alias', id: 'alias' },
+  { label: '', id: 'delete' }
 ]
