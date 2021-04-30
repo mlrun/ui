4ded434a620ed9cea03d5756b0a3584cbb44e4c6
mariana-furyk <mariana.furyk@globallogic.com>
2021-04-30 17:39:20 +0300

    removed comments

--- src/utils/generateArtifacts.js
+++ src/utils/generateArtifacts.js
@@ -1,6 +1,4 @@
-// import parseTargetPath from './parseTargetPath'
 import { generateArtifactPreviewData } from './generateArtifactPreviewData'
-
 import { maxBy, flatten } from 'lodash'
 
 export const generateArtifacts = artifacts =>
@@ -16,30 +14,17 @@ export const generateArtifacts = artifacts =>
           generatedArtifacts = generatedArtifacts.map(generatedArtifact => {
             let item = { ...generatedArtifact }
 
-            // item.target_path = parseTargetPath(generatedArtifact.target_path)
-
             if (generatedArtifact.extra_data) {
               const generatedPreviewData = generateArtifactPreviewData(
                 generatedArtifact.extra_data
               )
 
               item.preview = generatedPreviewData.preview
-              //
-              // @erann March 2, 2021
-              // Not sure why we have this. Commenting out for now.
 
               if (generatedPreviewData.extraDataPath) {
                 generatedArtifact.target_path =
                   generatedPreviewData.extraDataPath
               }
-
-              // @erann March 2, 2021
-              // Not sure why we have this. Commenting out for now.
-              //
-              // if (generatedPreviewData.extraDataPath) {
-              //   generatedArtifact.target_path.path =
-              //     generatedPreviewData.extraDataPath
-              // }
             } else {
               item.preview ??= []
             }
