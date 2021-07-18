908e1c62aaba143f4074911c83841cb0e4361513
iurii-vatazhyshyn <74406479+iurii-vatazhyshyn@users.noreply.github.com>
2021-07-18 15:37:07 +0300

    Fix [Feature sets] Transformations: text overflows (#687)

--- src/components/DetailsTransformations/ConfigSource/ConfigSource.js
+++ src/components/DetailsTransformations/ConfigSource/ConfigSource.js
@@ -6,6 +6,8 @@ import cronstrue from 'cronstrue'
 import { ReactComponent as Arrow } from '../../../images/arrow.svg'
 import Accordion from '../../../common/Accordion/Accordion'
 import ChipCell from '../../../common/ChipCell/ChipCell'
+import TextTooltipTemplate from '../../../elements/TooltipTemplate/TextTooltipTemplate'
+import Tooltip from '../../../common/Tooltip/Tooltip'
 
 const ConfigSource = ({ selectedItem }) => {
   return (
@@ -18,11 +20,27 @@ const ConfigSource = ({ selectedItem }) => {
       <div className="config-item__content">
         <div className="row">
           <div className="row-label">Kind:</div>
-          <div className="row-value">{selectedItem.source?.kind}</div>
+          <div className="row-value data-ellipsis">
+            <Tooltip
+              template={
+                <TextTooltipTemplate text={selectedItem.source?.kind} />
+              }
+            >
+              {selectedItem.source?.kind}
+            </Tooltip>
+          </div>
         </div>
         <div className="row">
           <div className="row-label">URL:</div>
-          <div className="row-value">{selectedItem.source?.path}</div>
+          <div className="row-value data-ellipsis">
+            <Tooltip
+              template={
+                <TextTooltipTemplate text={selectedItem.source?.path} />
+              }
+            >
+              {selectedItem.source?.path}
+            </Tooltip>
+          </div>
         </div>
         <div className="row">
           <div className="row-label">Attributes:</div>
@@ -38,22 +56,57 @@ const ConfigSource = ({ selectedItem }) => {
         </div>
         <div className="row">
           <div className="row-label">Workers:</div>
-          <div className="row-value">{selectedItem.source?.workers}</div>
+          <div className="row-value data-ellipsis">
+            <Tooltip
+              template={
+                <TextTooltipTemplate text={selectedItem.source?.workers} />
+              }
+            >
+              {selectedItem.source?.workers}
+            </Tooltip>
+          </div>
         </div>
         <div className="row">
           <div className="row-label">Schedule:</div>
-          <div className="row-value">
-            {selectedItem.source?.schedule &&
-              cronstrue.toString(selectedItem.source?.schedule)}
+          <div className="row-value data-ellipsis">
+            <Tooltip
+              template={
+                <TextTooltipTemplate
+                  text={
+                    selectedItem.source?.schedule &&
+                    cronstrue.toString(selectedItem.source?.schedule)
+                  }
+                />
+              }
+            >
+              {selectedItem.source?.schedule &&
+                cronstrue.toString(selectedItem.source?.schedule)}
+            </Tooltip>
           </div>
         </div>
         <div className="row">
           <div className="row-label">Key column:</div>
-          <div className="row-value">{selectedItem.source?.key_column}</div>
+          <div className="row-value data-ellipsis">
+            <Tooltip
+              template={
+                <TextTooltipTemplate text={selectedItem.source?.key_column} />
+              }
+            >
+              {selectedItem.source?.key_column}
+            </Tooltip>
+          </div>
         </div>
         <div className="row">
           <div className="row-label">Time column:</div>
-          <div className="row-value">{selectedItem.source?.time_column}</div>
+          <div className="row-value data-ellipsis">
+            <Tooltip
+              template={
+                <TextTooltipTemplate text={selectedItem.source?.time_column} />
+              }
+            >
+              {selectedItem.source?.time_column}
+            </Tooltip>
+          </div>
         </div>
       </div>
     </Accordion>
