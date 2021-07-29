2d215d12c034438fdbfbc4d4e13977a723870bd4
mariana-furyk <mariana.furyk@globallogic.com>
2021-07-29 11:39:05 +0300

    [Feature set panel] Entities have space in their name

--- src/components/FeatureSetsPanel/FeatureSetsPanelSchema/FeatureSetsPanelSchema.js
+++ src/components/FeatureSetsPanel/FeatureSetsPanelSchema/FeatureSetsPanelSchema.js
@@ -20,7 +20,7 @@ const FeatureSetsPanelSchema = ({
   const handleEntitiesOnBlur = () => {
     const entitiesArray = data.entities
       .split(',')
-      .map(entity => ({ name: entity, value_type: 'str' }))
+      .map(entity => ({ name: entity.trim(), value_type: 'str' }))
 
     if (
       data.entities.length > 0 &&
