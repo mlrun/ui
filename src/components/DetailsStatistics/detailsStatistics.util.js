e54e594a75caf82c9ca6f65720921d9819f730cd
mariana-furyk <mariana.furyk@globallogic.com>
2021-08-03 17:04:29 +0300

    [Feature vectors] Statistics: added Histogram

--- src/components/DetailsStatistics/detailsStatistics.util.js
+++ src/components/DetailsStatistics/detailsStatistics.util.js
@@ -7,82 +7,23 @@ import { ReactComponent as Primary } from '../../images/ic-key.svg'
 import { ReactComponent as LabelColumn } from '../../images/ic_target-with-dart.svg'
 
 export const generateStatistics = selectedItem => {
-  return selectedItem.entities
-    ? generateStatisticFromEntities(selectedItem)
-    : generateStatisticFromFeatures(selectedItem)
-}
+  const content = selectedItem.entities
+    ? selectedItem.entities
+        .map(item => ({ ...item, entity: 'entity' }))
+        .concat(selectedItem.features)
+    : selectedItem.features
 
-export const generateStatisticFromEntities = selectedItem => {
-  return selectedItem.entities
-    .map(item => ({ ...item, entity: 'entity' }))
-    .concat(selectedItem.features)
-    .map(item => {
-      return {
-        entityIcon: {
-          value: (
-            <Tooltip template={<TextTooltipTemplate text="Entity" />}>
-              <Primary />
-            </Tooltip>
-          ),
-          type: 'icon',
-          hidden: !item.entity
-        },
-        labelColumnIcon: {
-          value: (
-            <Tooltip template={<TextTooltipTemplate text="Label column" />}>
-              <LabelColumn />
-            </Tooltip>
-          ),
-          type: 'icon',
-          hidden: selectedItem.label_column !== item.name
-        },
-        name: {
-          value: item.name ?? '',
-          type: 'text'
-        },
-        count: {
-          value: selectedItem?.stats?.[item.name]?.count ?? '',
-          type: 'text'
-        },
-        mean: {
-          value: selectedItem?.stats?.[item.name]?.mean ?? '',
-          type: 'text'
-        },
-        std: {
-          value: selectedItem?.stats?.[item.name]?.std?.toFixed(8) ?? '',
-          type: 'text'
-        },
-        min: {
-          value: selectedItem?.stats?.[item.name]?.min ?? '',
-          type: 'text'
-        },
-        max: {
-          value: selectedItem?.stats?.[item.name]?.max ?? '',
-          type: 'text'
-        },
-        unique: {
-          value: selectedItem?.stats?.[item.name]?.unique ?? '',
-          type: 'text'
-        },
-        top: {
-          value: selectedItem?.stats?.[item.name]?.top ?? '',
-          type: 'text'
-        },
-        freq: {
-          value: selectedItem?.stats?.[item.name]?.freq ?? '',
-          type: 'text'
-        },
-        histogram: {
-          value: selectedItem?.stats?.[item.name]?.hist ?? [[], []],
-          type: 'chart'
-        }
-      }
-    })
-}
-
-export const generateStatisticFromFeatures = selectedItem => {
-  return selectedItem.features.map(item => {
+  return content.map(item => {
     return {
+      entityIcon: {
+        value: (
+          <Tooltip template={<TextTooltipTemplate text="Entity" />}>
+            <Primary />
+          </Tooltip>
+        ),
+        type: 'icon',
+        hidden: !item.entity
+      },
       labelColumnIcon: {
         value: (
           <Tooltip template={<TextTooltipTemplate text="Label column" />}>
@@ -127,6 +68,10 @@ export const generateStatisticFromFeatures = selectedItem => {
       freq: {
         value: selectedItem?.stats?.[item.name]?.freq ?? '',
         type: 'text'
+      },
+      histogram: {
+        value: selectedItem?.stats?.[item.name]?.hist ?? [[], []],
+        type: 'chart'
       }
     }
   })
