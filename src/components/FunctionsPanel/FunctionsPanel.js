b01077580081d40dcddc7e6dcf41d880282ab17c
iurii-vatazhyshyn <74406479+iurii-vatazhyshyn@users.noreply.github.com>
2021-08-31 20:12:41 +0300

    Impl [Function panel] Confirm overwrite when name already exists (#773)

--- src/components/FunctionsPanel/FunctionsPanel.js
+++ src/components/FunctionsPanel/FunctionsPanel.js
@@ -15,6 +15,7 @@ const FunctionsPanel = ({
   createFunctionSuccess,
   defaultData,
   deployFunction,
+  getFunction,
   handleDeployFunctionFailure,
   handleDeployFunctionSuccess,
   project,
@@ -25,6 +26,7 @@ const FunctionsPanel = ({
   setNewFunction,
   setNewFunctionProject
 }) => {
+  const [confirmData, setConfirmData] = useState(null)
   const [validation, setValidation] = useState({
     isNameValid: true,
     isHandlerValid: true,
@@ -96,6 +98,20 @@ const FunctionsPanel = ({
     )
   }
 
+  const createFunction = deploy => {
+    createNewFunction(project, functionsStore.newFunction).then(result => {
+      if (deploy) {
+        return handleDeploy(functionsStore.newFunction)
+      }
+
+      createFunctionSuccess().then(() => {
+        history.push(
+          `/projects/${project}/functions/${result.data.hash_key}/overview`
+        )
+      })
+    })
+  }
+
   const handleSave = deploy => {
     if (validation.isNameValid && validation.isHandlerValid) {
       if (functionsStore.newFunction.metadata.name.length === 0) {
@@ -110,17 +126,26 @@ const FunctionsPanel = ({
         removeFunctionsError()
       }
 
-      createNewFunction(project, functionsStore.newFunction).then(result => {
-        if (deploy) {
-          return handleDeploy(functionsStore.newFunction)
-        }
-
-        createFunctionSuccess().then(() => {
-          history.push(
-            `/projects/${project}/functions/${result.data.hash_key}/overview`
-          )
+      getFunction(project, functionsStore.newFunction.metadata.name)
+        .then(() => {
+          setConfirmData({
+            title: `Overwrite function "${functionsStore.newFunction.metadata.name}"?`,
+            description:
+              'The specified function name is already used by another function. Overwrite the other function with this one, or cancel and give this function another name?',
+            btnCancelLabel: 'Cancel',
+            btnCancelVariant: 'label',
+            btnConfirmLabel: 'Overwrite',
+            btnConfirmVariant: 'secondary',
+            rejectHandler: () => setConfirmData(null),
+            confirmHandler: () => {
+              createFunction(deploy)
+              setConfirmData(null)
+            }
+          })
         })
-      })
+        .catch(() => {
+          createFunction(deploy)
+        })
     }
   }
 
@@ -138,6 +163,7 @@ const FunctionsPanel = ({
     <FunctionsPanelView
       checkValidation={checkValidation()}
       closePanel={closePanel}
+      confirmData={confirmData}
       defaultData={defaultData ?? {}}
       error={functionsStore.error}
       handleSave={handleSave}
--- src/components/FunctionsPanel/FunctionsPanelView.js
+++ src/components/FunctionsPanel/FunctionsPanelView.js
@@ -11,6 +11,7 @@ import FunctionsPanelEnvironmentVariables from '../../elements/FunctionsPanelEnv
 import Button from '../../common/Button/Button'
 import ErrorMessage from '../../common/ErrorMessage/ErrorMessage'
 import FunctionsPanelSecrets from '../../elements/FunctionsPanelSecrets/FunctionsPanelSecrets'
+import PopUpDialog from '../../common/PopUpDialog/PopUpDialog'
 import { FUNCTION_PANEL_MODE } from '../../types'
 
 import { ReactComponent as Arrow } from '../../images/arrow.svg'
@@ -20,6 +21,7 @@ import './functionsPanel.scss'
 const FunctionsPanelView = ({
   checkValidation,
   closePanel,
+  confirmData,
   defaultData,
   error,
   handleSave,
@@ -30,99 +32,121 @@ const FunctionsPanelView = ({
   validation
 }) => {
   return (
-    <div className="new-item-side-panel-container">
-      <div className="functions-panel new-item-side-panel">
-        {loading && <Loader />}
-        <FunctionsPanelTitle closePanel={closePanel} />
-        <div className="new-item-side-panel__body">
-          <Accordion
-            accordionClassName="new-item-side-panel__accordion"
-            icon={<Arrow />}
-            iconClassName="new-item-side-panel__expand-icon"
-            openByDefault
-          >
-            <FunctionsPanelGeneral
-              defaultData={defaultData}
-              isNameValid={validation.isNameValid}
-              mode={mode}
-              setNameValid={setValidation}
+    <>
+      {confirmData && (
+        <PopUpDialog
+          headerText={confirmData.title}
+          closePopUp={confirmData.rejectHandler}
+        >
+          <div>{confirmData.description}</div>
+          <div className="pop-up-dialog__footer-container">
+            <Button
+              label={confirmData.btnCancelLabel}
+              onClick={confirmData.rejectHandler}
+              variant={confirmData.btnCancelVariant}
             />
-          </Accordion>
-          <Accordion
-            accordionClassName="new-item-side-panel__accordion"
-            icon={<Arrow />}
-            iconClassName="new-item-side-panel__expand-icon"
-            openByDefault
-          >
-            <FunctionsPanelCode
-              defaultData={defaultData}
-              isHandlerValid={validation.isHandlerValid}
-              setHandlerValid={setValidation}
+            <Button
+              label={confirmData.btnConfirmLabel}
+              onClick={confirmData.confirmHandler}
+              variant={confirmData.btnConfirmVariant}
             />
-          </Accordion>
-          <Accordion
-            accordionClassName="new-item-side-panel__accordion"
-            icon={<Arrow />}
-            iconClassName="new-item-side-panel__expand-icon"
-            openByDefault
-          >
-            <FunctionsPanelResources
-              defaultData={defaultData}
-              mode={mode}
-              setValidation={setValidation}
-              validation={validation}
-            />
-          </Accordion>
-          <Accordion
-            accordionClassName="new-item-side-panel__accordion"
-            icon={<Arrow />}
-            iconClassName="new-item-side-panel__expand-icon"
-            openByDefault
-          >
-            <FunctionsPanelEnvironmentVariables />
-          </Accordion>
-          <Accordion
-            accordionClassName="new-item-side-panel__accordion hidden"
-            icon={<Arrow />}
-            iconClassName="new-item-side-panel__expand-icon"
-            openByDefault
-          >
-            <FunctionsPanelSecrets />
-          </Accordion>
-          <div className="new-item-side-panel__buttons-container">
-            {error && (
-              <ErrorMessage
-                closeError={() => {
-                  if (error) {
-                    removeFunctionsError()
-                  }
-                }}
-                message={error}
+          </div>
+        </PopUpDialog>
+      )}
+      <div className="new-item-side-panel-container">
+        <div className="functions-panel new-item-side-panel">
+          {loading && <Loader />}
+          <FunctionsPanelTitle closePanel={closePanel} />
+          <div className="new-item-side-panel__body">
+            <Accordion
+              accordionClassName="new-item-side-panel__accordion"
+              icon={<Arrow />}
+              iconClassName="new-item-side-panel__expand-icon"
+              openByDefault
+            >
+              <FunctionsPanelGeneral
+                defaultData={defaultData}
+                isNameValid={validation.isNameValid}
+                mode={mode}
+                setNameValid={setValidation}
               />
-            )}
-            <Button
-              className="btn_cancel"
-              variant="label"
-              label="Cancel"
-              onClick={closePanel}
-            />
-            <Button
-              className="btn_save"
-              disabled={!checkValidation}
-              variant="tertiary"
-              label="Save"
-              onClick={() => handleSave()}
-            />
-            <Button
-              variant="secondary"
-              label="Deploy"
-              onClick={() => handleSave(true)}
-              disabled={!checkValidation}
-            />
+            </Accordion>
+            <Accordion
+              accordionClassName="new-item-side-panel__accordion"
+              icon={<Arrow />}
+              iconClassName="new-item-side-panel__expand-icon"
+              openByDefault
+            >
+              <FunctionsPanelCode
+                defaultData={defaultData}
+                isHandlerValid={validation.isHandlerValid}
+                setHandlerValid={setValidation}
+              />
+            </Accordion>
+            <Accordion
+              accordionClassName="new-item-side-panel__accordion"
+              icon={<Arrow />}
+              iconClassName="new-item-side-panel__expand-icon"
+              openByDefault
+            >
+              <FunctionsPanelResources
+                defaultData={defaultData}
+                mode={mode}
+                setValidation={setValidation}
+                validation={validation}
+              />
+            </Accordion>
+            <Accordion
+              accordionClassName="new-item-side-panel__accordion"
+              icon={<Arrow />}
+              iconClassName="new-item-side-panel__expand-icon"
+              openByDefault
+            >
+              <FunctionsPanelEnvironmentVariables />
+            </Accordion>
+            <Accordion
+              accordionClassName="new-item-side-panel__accordion hidden"
+              icon={<Arrow />}
+              iconClassName="new-item-side-panel__expand-icon"
+              openByDefault
+            >
+              <FunctionsPanelSecrets />
+            </Accordion>
+            <div className="new-item-side-panel__buttons-container">
+              {error && (
+                <ErrorMessage
+                  closeError={() => {
+                    if (error) {
+                      removeFunctionsError()
+                    }
+                  }}
+                  message={error}
+                />
+              )}
+              <Button
+                className="btn_cancel"
+                variant="label"
+                label="Cancel"
+                onClick={closePanel}
+              />
+              <Button
+                className="btn_save"
+                disabled={!checkValidation}
+                variant="tertiary"
+                label="Save"
+                onClick={() => handleSave()}
+              />
+              <Button
+                variant="secondary"
+                label="Deploy"
+                onClick={() => handleSave(true)}
+                disabled={!checkValidation}
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
@@ -134,6 +158,7 @@ FunctionsPanelView.defaultProps = {
 FunctionsPanelView.propTypes = {
   checkValidation: PropTypes.bool.isRequired,
   closePanel: PropTypes.func.isRequired,
+  confirmData: PropTypes.shape({}),
   defaultData: PropTypes.shape({}),
   error: PropTypes.string,
   handleSave: PropTypes.func.isRequired,
