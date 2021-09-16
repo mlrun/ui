d2a0544f198cc3339a0f2b75241efc51cd3e5f5d
iurii-vatazhyshyn <74406479+iurii-vatazhyshyn@users.noreply.github.com>
2021-09-16 17:00:12 +0300

    Fix [Job Panel] Volumes not reflected properly (#795)

--- src/components/FunctionsPage/Functions.js
+++ src/components/FunctionsPage/Functions.js
@@ -429,7 +429,9 @@ const Functions = ({
             name: editableItem.name,
             tag: editableItem.tag,
             functions: functionsStore.functions.filter(
-              func => func.metadata.name === editableItem.name
+              func =>
+                func.metadata.name === editableItem.name &&
+                func.metadata.hash === editableItem.hash
             )
           }}
           match={match}
