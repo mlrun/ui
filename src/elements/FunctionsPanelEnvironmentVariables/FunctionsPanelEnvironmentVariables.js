110bb2888154a295cdf43784f37665662a0c508e
illia-prokopchuk <78905712+illia-prokopchuk@users.noreply.github.com>
2021-10-21 17:26:48 +0300

    Impl [Function panel] Environment Variables: improve performance (#885)

--- src/elements/FunctionsPanelEnvironmentVariables/FunctionsPanelEnvironmentVariables.js
+++ src/elements/FunctionsPanelEnvironmentVariables/FunctionsPanelEnvironmentVariables.js
@@ -23,17 +23,12 @@ const FunctionsPanelEnvironmentVariables = ({
     if (isDemoMode) {
       const generatedVariable = generateEnvVariable(env)
 
-      setEnvVariables(state => [
-        ...state,
-        ...parseEnvVariables([generatedVariable])
-      ])
       setNewFunctionEnv([
         ...functionsStore.newFunction.spec.env,
         generatedVariable
       ])
     } else {
       setNewFunctionEnv([...envVariables, { name: env.key, value: env.value }])
-      setEnvVariables([...envVariables, { name: env.key, value: env.value }])
     }
   }
 
@@ -63,12 +58,10 @@ const FunctionsPanelEnvironmentVariables = ({
       const generatedVariables = env.map(item => generateEnvVariable(item))
 
       setNewFunctionEnv([...generatedVariables])
-      setEnvVariables([...parseEnvVariables(generatedVariables)])
     } else {
       const newData = envVariables.filter((_, index) => index !== env)
 
       setNewFunctionEnv([...newData])
-      setEnvVariables([...newData])
     }
   }
 
