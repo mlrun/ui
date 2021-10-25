e34302a1eb72dfcf3fe2385981b004303722c2a3
Dmytro <90618337+dmytro-panchuk@users.noreply.github.com>
2021-10-25 16:28:19 +0300

    Fix [Projects] Enter key on creation closes the pop-up (#895)

--- src/components/ProjectsPage/CreateProjectDialog/CreateProjectDialog.js
+++ src/components/ProjectsPage/CreateProjectDialog/CreateProjectDialog.js
@@ -69,6 +69,7 @@ const CreateProjectDialog = ({
             />
           )}
           <Button
+            type="button"
             disabled={projectStore.loading}
             variant={TERTIARY_BUTTON}
             label="Cancel"
