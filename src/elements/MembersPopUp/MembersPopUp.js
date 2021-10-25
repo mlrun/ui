07ba126d46b3f732a301815d60a2943165105936
ilan7empest <therecanbeonly1.1984@gmail.com>
2021-10-25 16:26:04 +0300

    ML-1286+ML-1287 - new memebers

--- src/elements/MembersPopUp/MembersPopUp.js
+++ src/elements/MembersPopUp/MembersPopUp.js
@@ -11,6 +11,10 @@ import Select from '../../common/Select/Select'
 import Tip from '../../common/Tip/Tip'
 import ChipInput from '../../common/ChipInput/ChipInput'
 
+import Tooltip from '../../common/Tooltip/Tooltip'
+import TextTooltipTemplate from '../../elements/TooltipTemplate/TextTooltipTemplate'
+import { ReactComponent as Close } from '../../images/close.svg'
+
 import projectsIguazioApi from '../../api/projects-iguazio-api'
 import { getRoleOptions, initialNewMembersRole } from './membersPopUp.util'
 import { membersActions } from './membersReducer'
@@ -233,7 +237,16 @@ const MembersPopUp = ({
             id: identity.id,
             subLabel: existingMember?.role ?? '',
             disabled: Boolean(existingMember),
-            icon: identity.type === 'user' ? <User /> : <Users />,
+            icon:
+              identity.type === 'user' ? (
+                <i data-identity-type="user">
+                  <User />
+                </i>
+              ) : (
+                <i data-identity-type="user_group">
+                  <Users />
+                </i>
+              ),
             ui: {
               type: identity.type
             }
@@ -284,7 +297,17 @@ const MembersPopUp = ({
         </div>
         {inviteNewMembers && (
           <div className="invite-new-members">
-            <div className="new-members-title">Invite new members</div>
+            <div className="new-members-title">
+              <span>Invite new members</span>
+              <div className="close-icon">
+                <Tooltip template={<TextTooltipTemplate text="Close" />}>
+                  <Close
+                    data-testid="pop-up-close-btn"
+                    onClick={() => setInviteNewMembers(false)}
+                  />
+                </Tooltip>
+              </div>
+            </div>
             <div className="new-members-row">
               <ChipInput
                 className="new-member-name"
