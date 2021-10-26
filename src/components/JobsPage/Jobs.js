55a28ee13071b0ef3b9faded8fd0bb159e531356
mariana-furyk <58301139+mariana-furyk@users.noreply.github.com>
2021-10-26 14:12:04 +0300

    Fix [Jobs] Filters are reset on opening job and going back to list(#899)

--- src/components/JobsPage/Jobs.js
+++ src/components/JobsPage/Jobs.js
@@ -80,6 +80,7 @@ const Jobs = ({
   const [editableItem, setEditableItem] = useState(null)
   const [selectedFunction, setSelectedFunction] = useState({})
   const [workflowsViewMode, setWorkflowsViewMode] = useState('graph')
+  const [dataIsLoaded, setDataIsLoaded] = useState(false)
   const isDemoMode = useDemoMode()
 
   const dispatch = useDispatch()
@@ -342,17 +343,11 @@ const Jobs = ({
             status: error?.response?.status || 400,
             id: Math.random(),
             message: 'Failed to fetch jobs',
-            retry: () => refreshJobs(filtersStore.filters)
+            retry: () => refreshJobs(filters)
           })
         })
     },
-    [
-      fetchJobs,
-      filtersStore.filters,
-      match.params.pageTab,
-      match.params.projectName,
-      setNotification
-    ]
+    [fetchJobs, match.params.pageTab, match.params.projectName, setNotification]
   )
 
   useEffect(() => {
@@ -455,10 +450,7 @@ const Jobs = ({
   ])
 
   useEffect(() => {
-    if (
-      (isEmpty(selectedJob) && !match.params.jobId) ||
-      workflowsViewMode === 'list'
-    ) {
+    if (isEmpty(selectedJob) && !match.params.jobId && !dataIsLoaded) {
       let filters = {}
 
       if (match.params.pageTab === MONITOR_JOBS_TAB) {
@@ -477,19 +469,23 @@ const Jobs = ({
       }
 
       refreshJobs(filters)
-
-      return () => {
-        setJobs([])
-      }
+      setDataIsLoaded(true)
     }
   }, [
+    dataIsLoaded,
     match.params.jobId,
     match.params.pageTab,
     refreshJobs,
     selectedJob,
-    setFilters,
-    workflowsViewMode
+    setFilters
   ])
+
+  useEffect(() => {
+    return () => {
+      setJobs([])
+      setDataIsLoaded(false)
+    }
+  }, [match.params.projectName, match.params.pageTab])
 
   const getWorkflows = useCallback(() => {
     fetchWorkflows(match.params.projectName)
