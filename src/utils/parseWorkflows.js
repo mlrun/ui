aea0c9aeaba5e9ba29dcaf6a95ab0d786a21e0e6
Dmytro <90618337+dmytro-panchuk@users.noreply.github.com>
2021-12-06 17:45:39 +0200

    [UI][Jobs workflow report] Missing workflows statuses in the workflows list presentation. (#966)
    [UI][Jobs workflow report] Missing workflows statuses in the workflows list presentation. (#966)

--- src/utils/parseWorkflows.js
+++ src/utils/parseWorkflows.js
@@ -1,7 +1,15 @@
+import getState from './getState'
+import { MONITOR_WORKFLOWS_TAB } from '../constants'
+
 export const parseWorkflows = workflows => {
   return workflows.map(workflow => {
     return {
       ...workflow,
+      state: getState(
+        workflow.status.toLowerCase(),
+        MONITOR_WORKFLOWS_TAB,
+        'workflow'
+      ),
       ui: {
         originalContent: workflow
       }
