1a9d33ca97ffc7947c5d59c41fa454b21282a2cb
ilan7empest <therecanbeonly1.1984@gmail.com>
2021-12-12 11:56:41 +0200

    removed defaultProps onclick

--- src/elements/ContentMenu/ContentMenu.js
+++ src/elements/ContentMenu/ContentMenu.js
@@ -55,13 +55,11 @@ const ContentMenu = ({ activeTab, match, screen, tabs, onClick }) => {
 
 ContentMenu.defaultProps = {
   activeTab: '',
-  onClick: () => {},
   tabs: []
 }
 
 ContentMenu.propTypes = {
   activeTab: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
   tabs: PropTypes.arrayOf(PropTypes.object).isRequired
 }
 
