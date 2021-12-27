7ad47f4fa9f586086dd65a28e524f391b8619178
iurii.vatazhyshyn <iurii.vatazhyshyn@globallogic.com>
2021-12-27 16:42:10 +0200

    Feature sets labels in the UI

--- src/utils/parseFeatureSets.js
+++ src/utils/parseFeatureSets.js
@@ -2,9 +2,9 @@ import getState from './getState'
 
 export const parseFeatureSets = featureSets =>
   featureSets.map(featureSet => ({
-    ...featureSet.metadata,
     ...featureSet.status,
     ...featureSet.spec,
+    ...featureSet.metadata,
     state: getState(featureSet.status.state),
     ui: {
       originalContent: featureSet
