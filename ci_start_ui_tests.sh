01c6a2fa8199ff89eef3affbb98fb3e6f87e400c
vasylvovkglo <vasyl.vovk@globallogic.com>
2022-01-24 15:20:48 +0200

    Shell script for running UI tests in headless mode added

---
+++ ci_start_ui_tests.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# set headless mode for browsers
+sed -i "s/headless: false/headless: true/g" tests/config.js
+
+# Synchronization with backend for mock
+echo "Synchronization with backend for mock"
+export BABEL_ENV=test
+export NODE_ENV=test
+cd tests/mockServer/
+node --require @babel/register backendSynchronizator.js
+cd ../..
+
+# setup mock endpoints
+printf "REACT_APP_MLRUN_API_URL=http://localhost:30000/mlrun-api-ingress.default-tenant.app.vmdev36.lab.iguazeng.com\n\
+REACT_APP_NUCLIO_API_URL=http://localhost:30000/nuclio-ingress.default-tenant.app.vmdev36.lab.iguazeng.com\n\
+REACT_APP_IGUAZIO_API_URL=http://localhost:30000/platform-api.default-tenant.app.vmdev36.lab.iguazeng.com\n\
+REACT_APP_NUCLIO_UI_URL=http://localhost:8070\n\
+REACT_APP_FUNCTION_CATALOG_URL=https://raw.githubusercontent.com/mlrun/functions/master\n" > .env.development.local
+
+# install dependencies
+echo "install dependencies"
+npm i
+
+# start UI test run
+echo "start UI test run"
+npm run start:regression
+
+
