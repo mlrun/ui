06ef1e5ec7e8e3545bf9b6d7fece2759d7896c86
vasylvovkglo <vasyl.vovk@globallogic.com>
2022-01-31 13:24:44 +0200

    DEVOPS-342 fix mock data

--- ci_start_ui_tests.sh
+++ ci_start_ui_tests.sh
@@ -4,13 +4,6 @@ sed -i "s/headless: false/headless: true/g" tests/config.js
 # install dependencies
 echo "install dependencies"
 npm i
-# Synchronization with backend for mock
-echo "Synchronization with backend for mock"
-export BABEL_ENV=test
-export NODE_ENV=test
-cd tests/mockServer/
-node --require @babel/register backendSynchronizator.js
-cd ../..
 # setup mock endpoints
 printf "REACT_APP_MLRUN_API_URL=http://localhost:30000/mlrun-api-ingress.default-tenant.app.vmdev36.lab.iguazeng.com\n\
 REACT_APP_NUCLIO_API_URL=http://localhost:30000/nuclio-ingress.default-tenant.app.vmdev36.lab.iguazeng.com\n\
