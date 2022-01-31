4c84f872d2a69f76abfafb8658a8853f91047c27
Ilank <63646693+ilan7empest@users.noreply.github.com>
2022-01-31 17:54:23 +0200

    Impl [tests] Sprint 133 (#1065)

--- tests/features/common/page-objects/project-settings.po.js
+++ tests/features/common/page-objects/project-settings.po.js
@@ -7,7 +7,7 @@ const tabSelector = {
   root: '.content-menu',
   header: {},
   body: {
-    root: 'ul.content-menu__list',
+    root: '.content-menu__list',
     row: {
       root: '.content-menu__item',
       fields: {
@@ -60,9 +60,7 @@ const secretsTable = {
 module.exports = {
   generalTab: {
     Project_Settings_Tab_Selector: commonTable(tabSelector),
-    Source_URL_Edit_Button: By.css(
-      '.settings__card-content .settings__source .settings__source-link'
-    ),
+    Source_URL_Edit_Button: By.css('.settings__card-content .settings__source'),
     Source_URL_Edit_Input: inputGroup(
       generateInputGroup(
         '.settings__card-content .settings__source .input-wrapper',
