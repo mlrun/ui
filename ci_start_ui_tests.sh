d1ae7b111f344de24bf16b8b514d90fd61fd7a0e
vasylvovkglo <vasyl.vovk@globallogic.com>
2022-02-01 14:40:25 +0200

    DEVOPS-342: fixed regression run for CI and added all dependencies

--- ci_start_ui_tests.sh
+++ ci_start_ui_tests.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env sh
+# install google-chrome and utilites
+yum update -y
+yum install -y screen
+curl https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -o google-chrome-stable_current_x86_64.rpm
+yum install google-chrome-stable_current_x86_64.rpm -y
 # set headless mode for browsers
 sed -i "s/headless: false/headless: true/g" tests/config.js
 # install dependencies
 echo "install dependencies"
 npm i
-# Synchronization with backend for mock
-echo "Synchronization with backend for mock"
-export BABEL_ENV=test
-export NODE_ENV=test
-cd tests/mockServer/
-node --require @babel/register backendSynchronizator.js
-cd ../..
 # setup mock endpoints
 printf "REACT_APP_MLRUN_API_URL=http://localhost:30000/mlrun-api-ingress.default-tenant.app.vmdev36.lab.iguazeng.com\n\
 REACT_APP_NUCLIO_API_URL=http://localhost:30000/nuclio-ingress.default-tenant.app.vmdev36.lab.iguazeng.com\n\
@@ -19,6 +17,7 @@ REACT_APP_NUCLIO_UI_URL=http://localhost:8070\n\
 REACT_APP_FUNCTION_CATALOG_URL=https://raw.githubusercontent.com/mlrun/functions/master\n" > .env.development.local
 # start UI test run
 echo "start UI test run"
-npm run start:regression
-
-
+screen -dm npm run mock-server
+screen -dm npm start
+npm run test:regression
+pkill screen
