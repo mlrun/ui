656a99c0570865d81e6217e96624d018b31eced9
iurii.vatazhyshyn <iurii.vatazhyshyn@globallogic.com>
2022-03-30 15:48:53 +0300

    [UI][Consumer group] Consumer Group report is empty

--- src/utils/parseV3ioStreams.js
+++ src/utils/parseV3ioStreams.js
@@ -3,9 +3,15 @@ import { map } from 'lodash'
 export const parseV3ioStreams = consumerGroups => {
   return map(consumerGroups, (consumerGroupData, functionStreamName) => {
     const [functionName, streamName] = functionStreamName.split('@')
+    let streamPath = consumerGroupData.streamPath
+
+    if (!streamPath.startsWith('/')) {
+      streamPath = `/${streamPath}`
+    }
 
     return {
       ...consumerGroupData,
+      streamPath,
       functionName,
       streamName,
       ui: {
