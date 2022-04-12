cf01cb5c9991eb7d6af50054b6c21312a2d9f6d0
ilan7empest <therecanbeonly1.1984@gmail.com>
2022-04-12 20:06:48 +0300

    Fix [Feature Store] Add to feature vector - Incorrect right panel height

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -72,7 +72,7 @@ const Table = ({
       calculatePanelHeight()
 
       document
-        .getElementById('main')
+        .querySelector('.main-wrapper')
         .addEventListener('scroll', calculatePanelHeight)
       window.addEventListener('resize', calculatePanelHeight)
     }
