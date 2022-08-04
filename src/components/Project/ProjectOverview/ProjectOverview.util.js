404fecfb4b566f33d76b947e266e564493fec2ef
Ilank <63646693+ilan7empest@users.noreply.github.com>
2022-08-04 16:57:04 +0300

    Impl [Project Overview] Register model wizard (project home) (#1364)

--- src/components/Project/ProjectOverview/ProjectOverview.util.js
+++ src/components/Project/ProjectOverview/ProjectOverview.util.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import RegisterArtifactModal from '../../RegisterArtifactModal/RegisterArtifactModal'
+import RegisterModelPopUp from '../../../elements/RegisterModelPopUp/RegisterModelPopUp'
 
 import { SECONDARY_BUTTON, TERTIARY_BUTTON } from 'igz-controls/constants'
 
@@ -222,7 +223,46 @@ export const getInitialCards = (projectName, navigate) => {
           id: 'registeramodel',
           icon: <RegisterModelIcon />,
           handleClick: () => ({
-            path: `${base_url}/models/models?openPanel=true`
+            component: RegisterModelPopUp,
+            props: {
+              actions: (formState, handleCloseModal) => [
+                {
+                  label: 'Cancel',
+                  onClick: () => handleCloseModal(),
+                  variant: TERTIARY_BUTTON
+                },
+                {
+                  disabled: formState.submitting || (formState.invalid && formState.submitFailed),
+                  label: 'Register',
+                  onClick: async () => {
+                    await formState.handleSubmit()
+                    if (!formState.invalid) {
+                      navigate(`${base_url}/models/models`)
+                    }
+                  },
+                  variant: SECONDARY_BUTTON
+                }
+              ],
+              // TODO: un-comment for 1.3
+              // [{
+              //   disabled: formState.submitting || (formState.invalid && formState.submitFailed),
+              //   label: 'Register and view',
+              //   onClick: async () => {
+              //   await formState.handleSubmit()
+              //   if (!formState.invalid) {
+              //     navigate(`${base_url}/datasets`)
+              //   }
+              // },
+              // {
+              //   disabled: formState.submitting || (formState.invalid && formState.submitFailed),
+              //   label: 'Register',
+              //   onClick: formState.handleSubmit,
+              //   variant: SECONDARY_BUTTON
+              // }],
+              projectName,
+              refresh: () => {}
+            },
+            type: 'modal'
           }),
           label: 'Register Model',
           tooltip: ''
