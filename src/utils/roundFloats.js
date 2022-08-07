28f222e3a542f1cd6a064960b5d4482260c17f7f
ilan7empest <therecanbeonly1.1984@gmail.com>
2022-08-07 16:45:43 +0300

    Fix [Jobs] State column is empty in Results tab

--- src/utils/roundFloats.js
+++ src/utils/roundFloats.js
@@ -1,7 +1,9 @@
 export const roundFloats = (value, precision) => {
   const floatNumber = parseFloat(value)
 
-  if (isNaN(floatNumber)) return ''
+  if (isNaN(floatNumber)) {
+    return value ? value : ''
+  }
 
   return parseFloat(floatNumber.toFixed(precision ?? 2))
 }
