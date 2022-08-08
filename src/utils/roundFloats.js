c42a9830b24f7d9850347c3f05350b0e9b37a6d2
dmytro <dmytro.panchuk@globallogic.com>
2022-08-08 14:29:00 +0300

    Fix [UI] Label value is not being displayed

--- src/utils/roundFloats.js
+++ src/utils/roundFloats.js
@@ -1,7 +1,7 @@
 export const roundFloats = (value, precision) => {
   const floatNumber = parseFloat(value)
 
-  if (isNaN(floatNumber)) return ''
+  if (isNaN(floatNumber)) return value
 
   return parseFloat(floatNumber.toFixed(precision ?? 2))
 }
