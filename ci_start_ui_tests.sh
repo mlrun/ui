fa64e40ffdf7cd10d6fa8e92ca99340dd6300118
eliyahu77 <root@linux-eliyahun.iguaz.io>
2022-08-24 16:32:08 +0300

    update shebang
    Signed-off-by: eliyahu77 <root@linux-eliyahun.iguaz.io>

--- ci_start_ui_tests.sh
+++ ci_start_ui_tests.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env sh
 # Copyright 2019 Iguazio
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-#!/usr/bin/env sh
 # install google-chrome and utilites
 yum update -y
 yum install -y screen
