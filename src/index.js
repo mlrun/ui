ea874e0a54200deba5f4100d2e8064589a267504
ilan7empest <therecanbeonly1.1984@gmail.com>
2022-09-05 16:11:28 +0300

    Fix [UI] blank screen on attempt to browse Projects

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ import ModalContainer from 'react-modal-promise'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './App'
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary'
 
 import * as serviceWorker from './serviceWorker'
 import { Provider } from 'react-redux'
@@ -37,7 +38,9 @@ fetch(`${process.env.PUBLIC_URL}/config.json`, { cache: 'no-store' })
     ReactDOM.render(
       <Provider store={toolkitStore}>
         <Router basename={process.env.PUBLIC_URL}>
-          <App />
+          <ErrorBoundary>
+            <App />
+          </ErrorBoundary>
           {createPortal(<ModalContainer />, document.getElementById('overlay_container'))}
         </Router>
       </Provider>,
