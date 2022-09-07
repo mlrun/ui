99cb333165cff4c4de4818644372c403f045febb
ilan7empest <therecanbeonly1.1984@gmail.com>
2022-09-07 14:01:18 +0300

    change text

--- src/components/ErrorBoundary/ErrorBoundary.js
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -38,7 +38,7 @@ class ErrorBoundary extends React.Component {
   render() {
     if (this.state.hasError) {
       // You can render any custom fallback UI
-      return <h1>Something went wrong. Try to refresh the page.</h1>
+      return <h1>Something went wrong.</h1>
     }
 
     return this.props.children
