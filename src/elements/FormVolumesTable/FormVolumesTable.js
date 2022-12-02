8ed4459641554c37e764c77bea017ea338b5d156
Andrii Mavdryk <andrii.mavdryk@globallogic.com>
2022-12-02 22:04:10 +0200

    Impl Revamp New Job Wizard - New layout (part13)

--- src/elements/FormVolumesTable/FormVolumesTable.js
+++ src/elements/FormVolumesTable/FormVolumesTable.js
@@ -1,3 +1,22 @@
+/*
+Copyright 2019 Iguazio Systems Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License") with
+an addition restriction as set forth herein. You may not use this
+file except in compliance with the License. You may obtain a copy of
+the License at http://www.apache.org/licenses/LICENSE-2.0.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the License for the specific language governing
+permissions and limitations under the License.
+
+In addition, you may not use the software for any purposes that are
+illegal under applicable law, and the grant of the foregoing license
+under the Apache 2.0 license is conditioned upon your compliance with
+such restriction.
+*/
 import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
@@ -13,13 +32,14 @@ import { V3IO_VOLUME_TYPE } from '../../constants'
 const FormVolumesTable = ({ disabled, fieldsPath, formState }) => {
   const tableClassNames = classnames('form-table', 'form-col-1', disabled && 'disabled')
   const {
-    editingItem,
     addNewRow,
     applyChanges,
+    bottomScrollRef,
     deleteRow,
     discardOrDelete,
+    editingItem,
     enterEditMode,
-    bottomScrollRef
+    isCurrentRowEditing
   } = useFormTable(formState)
 
   return (
@@ -52,6 +72,7 @@ const FormVolumesTable = ({ disabled, fieldsPath, formState }) => {
                     fields={fields}
                     fieldsPath={fieldsPath}
                     index={index}
+                    isCurrentRowEditing={isCurrentRowEditing}
                     key={rowPath}
                     rowPath={rowPath}
                     setFieldValue={formState.form.change}
