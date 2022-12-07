34bda3dceffdca15140cfd2e2ee4fbf144a32633
Ilank <63646693+ilan7empest@users.noreply.github.com>
2022-12-07 15:15:25 +0200

    Fix [UI] Models screen - show iteration checkbox rename (#1489)

--- src/components/ModelsPage/Models/models.util.js
+++ src/components/ModelsPage/Models/models.util.js
@@ -40,7 +40,7 @@ export const filters = [
   { type: TAG_FILTER, label: 'Version tag:' },
   { type: NAME_FILTER, label: 'Name:' },
   { type: LABELS_FILTER, label: 'Labels:' },
-  { type: ITERATIONS_FILTER, label: 'Show iterations' }
+  { type: ITERATIONS_FILTER, label: 'Show best iteration only' }
 ]
 
 export const infoHeaders = [
