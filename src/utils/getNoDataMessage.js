5624571b4464d3e2cb8c61857a936d39f49a631a
ilan7empest <therecanbeonly1.1984@gmail.com>
2022-12-13 12:41:07 +0200

    Fix [UI] The warning 'No data to show' should be augmented with the applied filter

--- src/utils/getNoDataMessage.js
+++ src/utils/getNoDataMessage.js
@@ -17,132 +17,151 @@ illegal under applicable law, and the grant of the foregoing license
 under the Apache 2.0 license is conditioned upon your compliance with
 such restriction.
 */
-import { isEqual} from 'lodash'
+import { isEqual } from 'lodash'
+import { formatDate } from '../utils/datePicker.util'
 import {
-    ADD_TO_FEATURE_VECTOR_TAB,
-    DATASETS_PAGE,
-    DATE_FILTER_ANY_TIME,
-    DATE_RANGE_TIME_FILTER,
-    FEATURE_SETS_TAB,
-    FEATURE_VECTORS_TAB,
-    FEATURES_TAB, FILES_PAGE, FUNCTIONS_PAGE,
-    GROUP_BY_FILTER,
-    GROUP_BY_NONE,
-    ITERATIONS_FILTER, JOBS_PAGE,
-    LABELS_FILTER, MODEL_ENDPOINTS_TAB, MODELS_TAB,
-    NAME_FILTER, REAL_TIME_PIPELINES_TAB,
-    SHOW_ITERATIONS,
-    SHOW_UNTAGGED_FILTER,
-    SHOW_UNTAGGED_ITEMS,
-    STATE_FILTER_ALL_ITEMS,
-    STATUS_FILTER,
-    TAG_FILTER,
-    TAG_FILTER_ALL_ITEMS
+  ADD_TO_FEATURE_VECTOR_TAB,
+  DATASETS_PAGE,
+  DATE_FILTER_ANY_TIME,
+  DATE_RANGE_TIME_FILTER,
+  FEATURE_SETS_TAB,
+  FEATURE_VECTORS_TAB,
+  FEATURES_TAB,
+  FILES_PAGE,
+  FUNCTIONS_PAGE,
+  GROUP_BY_FILTER,
+  GROUP_BY_NONE,
+  ITERATIONS_FILTER,
+  JOBS_PAGE,
+  LABELS_FILTER,
+  MODEL_ENDPOINTS_TAB,
+  MODELS_TAB,
+  NAME_FILTER,
+  REAL_TIME_PIPELINES_TAB,
+  SHOW_ITERATIONS,
+  SHOW_UNTAGGED_FILTER,
+  SHOW_UNTAGGED_ITEMS,
+  STATE_FILTER_ALL_ITEMS,
+  STATUS_FILTER,
+  TAG_FILTER,
+  TAG_FILTER_ALL_ITEMS
 } from '../constants'
 
 const messageNamesList = {
-    [ADD_TO_FEATURE_VECTOR_TAB]: {
-        single: 'Feature',
-        plural: 'Features'
-    },
-    [DATASETS_PAGE]: {
-        single: 'Dataset',
-        plural: 'Datasets'
-    },
-    [FEATURE_SETS_TAB]: {
-        single: 'Feature-Set',
-        plural: 'Feature-Sets'
-    },
-    [FEATURE_VECTORS_TAB]: {
-        single: 'Feature-Vector',
-        plural: 'Feature-Vectors'
-    },
-    [FEATURES_TAB]: {
-        single: 'Feature',
-        plural: 'Features'
-    },
-    [FILES_PAGE]: {
-        single: 'File',
-        plural: 'Files'
-    },
-    [FUNCTIONS_PAGE]: {
-        single: 'Function',
-        plural: 'Functions'
-    },
-    [JOBS_PAGE]: {
-        single: 'Job',
-        plural: 'Jobs'
-    },
-    [MODELS_TAB]: {
-        single: 'Model',
-        plural: 'Models'
-    },
-    [MODEL_ENDPOINTS_TAB]: {
-        single: 'Model-endpoint',
-        plural: 'Model-endpoints'
-    },
-    [REAL_TIME_PIPELINES_TAB]: {
-        single: 'Real-time pipeline',
-        plural: 'Real-time pipelines'
-    },
-    default: ''
+  [ADD_TO_FEATURE_VECTOR_TAB]: {
+    single: 'Feature',
+    plural: 'Features'
+  },
+  [DATASETS_PAGE]: {
+    single: 'Dataset',
+    plural: 'Datasets'
+  },
+  [FEATURE_SETS_TAB]: {
+    single: 'Feature-Set',
+    plural: 'Feature-Sets'
+  },
+  [FEATURE_VECTORS_TAB]: {
+    single: 'Feature-Vector',
+    plural: 'Feature-Vectors'
+  },
+  [FEATURES_TAB]: {
+    single: 'Feature',
+    plural: 'Features'
+  },
+  [FILES_PAGE]: {
+    single: 'File',
+    plural: 'Files'
+  },
+  [FUNCTIONS_PAGE]: {
+    single: 'Function',
+    plural: 'Functions'
+  },
+  [JOBS_PAGE]: {
+    single: 'Job',
+    plural: 'Jobs'
+  },
+  [MODELS_TAB]: {
+    single: 'Model',
+    plural: 'Models'
+  },
+  [MODEL_ENDPOINTS_TAB]: {
+    single: 'Model-endpoint',
+    plural: 'Model-endpoints'
+  },
+  [REAL_TIME_PIPELINES_TAB]: {
+    single: 'Real-time pipeline',
+    plural: 'Real-time pipelines'
+  },
+  default: ''
 }
 
 export const getNoDataMessage = (filtersStore, filters, page, tab) => {
-    const messageNames = messageNamesList[tab] || messageNamesList[page] || messageNamesList.default
+  const messageNames = messageNamesList[tab] || messageNamesList[page] || messageNamesList.default
 
-    if (!messageNames) {
-        return 'No data to show'
-    } else {
-        const changedFiltersList = getChangedFiltersList(filters, filtersStore)
+  if (!messageNames) {
+    return 'No data to show'
+  } else {
+    const changedFiltersList = getChangedFiltersList(filters, filtersStore)
 
-        return changedFiltersList.length > 0 ?
-            generateNoEntriesFoundMessage(changedFiltersList, filtersStore, messageNames) :
-            generateEmptyListMessage(messageNames, tab)
-    }
+    return changedFiltersList.length > 0
+      ? generateNoEntriesFoundMessage(changedFiltersList, filtersStore, messageNames)
+      : generateEmptyListMessage(messageNames, tab)
+  }
 }
 
 const generateEmptyListMessage = (messageNames, tab) => {
-    if ([MODEL_ENDPOINTS_TAB, REAL_TIME_PIPELINES_TAB].includes(tab)) {
-        return 'No data to show'
-    }
+  if ([MODEL_ENDPOINTS_TAB, REAL_TIME_PIPELINES_TAB].includes(tab)) {
+    return 'No data to show'
+  }
 
-    if ([FEATURES_TAB, ADD_TO_FEATURE_VECTOR_TAB].includes(tab)) {
-        return 'No features yet. Go to "Feature Sets" tab to create your first Feature Set.'
-    }
+  if ([FEATURES_TAB, ADD_TO_FEATURE_VECTOR_TAB].includes(tab)) {
+    return 'No features yet. Go to "Feature Sets" tab to create your first Feature Set.'
+  }
 
-    return `No ${messageNames.plural} yet. Create your first ${messageNames.single} now.`
+  return `No ${messageNames.plural} yet. Create your first ${messageNames.single} now.`
 }
 
 const generateNoEntriesFoundMessage = (changedFilters, filtersStore, messageNames) => {
-    return changedFilters.reduce((message, filter, index) => {
-        const label = [ITERATIONS_FILTER, SHOW_UNTAGGED_ITEMS].includes(filter.type) ? `${filter.label}:` :
-                      filter.type === DATE_RANGE_TIME_FILTER ? 'Date:' : filter.label
-        const value = [ITERATIONS_FILTER, SHOW_UNTAGGED_ITEMS].includes(filter.type) ? 'true' :
-                      filter.type === DATE_RANGE_TIME_FILTER ? 'Selected time' : filter.type === STATUS_FILTER ?
-                      filtersStore['state'] : filtersStore[filter.type]
-        const isLastElement = index === changedFilters.length - 1
+  return changedFilters.reduce((message, filter, index) => {
+    const label = [ITERATIONS_FILTER, SHOW_UNTAGGED_ITEMS].includes(filter.type)
+      ? `${filter.label}:`
+      : filter.type === DATE_RANGE_TIME_FILTER
+      ? 'Date:'
+      : filter.label
+    const value = [ITERATIONS_FILTER, SHOW_UNTAGGED_ITEMS].includes(filter.type)
+      ? 'true'
+      : filter.type === DATE_RANGE_TIME_FILTER
+      ? formatDate(
+          true,
+          true,
+          '/',
+          filtersStore.dates.value[0],
+          filtersStore.dates.value[1] ?? new Date()
+        )
+      : filter.type === STATUS_FILTER
+      ? filtersStore['state']
+      : filtersStore[filter.type]
+    const isLastElement = index === changedFilters.length - 1
 
-        return message + `${label} ${value}${isLastElement ? '"' : ', '}`
-    }, `There is no ${messageNames.plural} data to show for "`)
+    return message + `${label} ${value}${isLastElement ? '"' : ', '}`
+  }, `There is no ${messageNames.plural} data to show for "`)
 }
 
 const getChangedFiltersList = (filters, filtersStore) => {
-    if (!filters || !filtersStore) {
-        return []
-    }
+  if (!filters || !filtersStore) {
+    return []
+  }
 
-    return filters.filter(({ type }) => {
-        return (
-            ((type === TAG_FILTER) &&
-                filtersStore.tag !== TAG_FILTER_ALL_ITEMS) ||
-            ((type === NAME_FILTER || type === LABELS_FILTER) && filtersStore[type].length > 0) ||
-            (type === STATUS_FILTER && filtersStore.state !== STATE_FILTER_ALL_ITEMS) ||
-            (type === DATE_RANGE_TIME_FILTER &&
-                !isEqual(filtersStore.dates.value, DATE_FILTER_ANY_TIME)) ||
-            (type === ITERATIONS_FILTER && filtersStore.iter === SHOW_ITERATIONS) ||
-            (type === SHOW_UNTAGGED_FILTER && filtersStore.showUntagged !== SHOW_UNTAGGED_ITEMS) ||
-            (type === GROUP_BY_FILTER && filtersStore.groupBy !== GROUP_BY_NONE)
-        )
-    })
+  return filters.filter(({ type }) => {
+    return (
+      (type === TAG_FILTER && filtersStore.tag !== TAG_FILTER_ALL_ITEMS) ||
+      ((type === NAME_FILTER || type === LABELS_FILTER) && filtersStore[type].length > 0) ||
+      (type === STATUS_FILTER && filtersStore.state !== STATE_FILTER_ALL_ITEMS) ||
+      (type === DATE_RANGE_TIME_FILTER &&
+        !isEqual(filtersStore.dates.value, DATE_FILTER_ANY_TIME)) ||
+      (type === ITERATIONS_FILTER && filtersStore.iter === SHOW_ITERATIONS) ||
+      (type === SHOW_UNTAGGED_FILTER && filtersStore.showUntagged !== SHOW_UNTAGGED_ITEMS) ||
+      (type === GROUP_BY_FILTER && filtersStore.groupBy !== GROUP_BY_NONE)
+    )
+  })
 }
