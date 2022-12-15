6ecdf4137c744dca6ad6fdf3f5785002628dfb14
ilan7empest <therecanbeonly1.1984@gmail.com>
2022-12-15 13:48:13 +0200

    change prop from isAsync to sync

--- src/elements/RegisterModelModal/RegisterModelModal.js
+++ src/elements/RegisterModelModal/RegisterModelModal.js
@@ -170,7 +170,7 @@ function RegisterModelModal({ actions, isOpen, onResolve, projectName, refresh }
           >
             <div className="form-row">
               <FormInput
-                isAsync
+                async
                 label="Name"
                 name="metadata.key"
                 required
