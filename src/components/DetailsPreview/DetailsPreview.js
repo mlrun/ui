806f55afa8628f12aaf9dfe940ed644f04c69769
ilan7empest <therecanbeonly1.1984@gmail.com>
2023-01-26 18:15:22 +0200

    use memo for popupButtonIsDisplayed

--- src/components/DetailsPreview/DetailsPreview.js
+++ src/components/DetailsPreview/DetailsPreview.js
@@ -17,7 +17,7 @@ illegal under applicable law, and the grant of the foregoing license
 under the Apache 2.0 license is conditioned upon your compliance with
 such restriction.
 */
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { isEqual } from 'lodash'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
@@ -41,12 +41,15 @@ const DetailsPreview = ({ artifact, handlePreview }) => {
   const previewRef = useRef({ current: {} })
   const params = useParams()
 
-  const isPopupButtonDisplayed =
-    artifact.target_path &&
-    (extraData.length > 0 || (!preview[0]?.error && !preview.every(item => item.hidden)))
+  const popupButtonIsDisplayed = useMemo(() => {
+    return (
+      artifact.target_path &&
+      (extraData.length > 0 || (!preview[0]?.error && !preview.every(item => item.hidden)))
+    )
+  }, [artifact.target_path, extraData.length, preview])
 
   const artifactsPreviewClassNames = classnames(
-    isPopupButtonDisplayed && 'artifact-preview__with-popout'
+    popupButtonIsDisplayed && 'artifact-preview__with-popout'
   )
 
   useEffect(() => {
@@ -82,7 +85,7 @@ const DetailsPreview = ({ artifact, handlePreview }) => {
 
   return (
     <div className="preview_container">
-      {isPopupButtonDisplayed && (
+      {popupButtonIsDisplayed && (
         <button onClick={() => handlePreview()} className="preview_popout">
           <Tooltip template={<TextTooltipTemplate text="Pop-out" />}>
             <Popout />
