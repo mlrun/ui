154b0a8df770b92c40a4069a54a9746d1686138a
ilan7empest <therecanbeonly1.1984@gmail.com>
2023-04-30 12:15:52 +0300

    Fix [Models] UI crash on attempt to open Models screen

--- src/utils/createArtifactsContent.js
+++ src/utils/createArtifactsContent.js
@@ -105,9 +105,11 @@ const createArtifactsRowData = artifact => {
 
 export const createModelsRowData = (artifact, project, frontendSpec, showExpandButton) => {
   const iter = isNaN(parseInt(artifact?.iter)) ? '' : ` #${artifact?.iter}`
-  const isTargetPathValid = frontendSpec.allowed_artifact_path_prefixes_list.some(prefix => {
-    return artifact.target_path?.startsWith?.(prefix)
-  })
+  const isTargetPathValid = frontendSpec?.allowed_artifact_path_prefixes_list
+    ? frontendSpec.allowed_artifact_path_prefixes_list.some(prefix => {
+        return artifact.target_path?.startsWith?.(prefix)
+      })
+    : false
 
   return {
     data: {
