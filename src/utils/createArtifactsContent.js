93ff0046dcd7497f1e53711e4d4aaa5ae8062365
Ilank <63646693+ilan7empest@users.noreply.github.com>
2023-05-09 12:35:59 +0300

    Fix [Models] UI crash on attempt to open Models screen `1.3.x` (#1707)

--- src/utils/createArtifactsContent.js
+++ src/utils/createArtifactsContent.js
@@ -104,11 +104,17 @@ const createArtifactsRowData = artifact => {
   }
 }
 
+const getIter = artifact => (isNaN(parseInt(artifact?.iter)) ? '' : ` #${artifact?.iter}`)
+const getIsTargetPathValid = (artifact, frontendSpec) =>
+  frontendSpec?.allowed_artifact_path_prefixes_list
+    ? frontendSpec.allowed_artifact_path_prefixes_list.some(prefix => {
+        return artifact.target_path?.startsWith?.(prefix)
+      })
+    : false
+
 export const createModelsRowData = (artifact, project, frontendSpec, showExpandButton) => {
-  const iter = isNaN(parseInt(artifact?.iter)) ? '' : ` #${artifact?.iter}`
-  const isTargetPathValid = frontendSpec.allowed_artifact_path_prefixes_list.some(prefix => {
-    return artifact.target_path?.startsWith?.(prefix)
-  })
+  const iter = getIter(artifact)
+  const isTargetPathValid = getIsTargetPathValid(artifact, frontendSpec)
 
   return {
     data: {
@@ -242,10 +248,8 @@ export const createModelsRowData = (artifact, project, frontendSpec, showExpandB
 }
 
 export const createFilesRowData = (artifact, project, frontendSpec, showExpandButton) => {
-  const iter = isNaN(parseInt(artifact?.iter)) ? '' : ` #${artifact?.iter}`
-  const isTargetPathValid = frontendSpec.allowed_artifact_path_prefixes_list.some(prefix => {
-    return artifact.target_path?.startsWith?.(prefix)
-  })
+  const iter = getIter(artifact)
+  const isTargetPathValid = getIsTargetPathValid(artifact, frontendSpec)
 
   return {
     data: {
@@ -488,10 +492,8 @@ export const createModelEndpointsRowData = (artifact, project) => {
 }
 
 export const createDatasetsRowData = (artifact, project, frontendSpec, showExpandButton) => {
-  const iter = isNaN(parseInt(artifact?.iter)) ? '' : ` #${artifact?.iter}`
-  const isTargetPathValid = frontendSpec.allowed_artifact_path_prefixes_list.some(prefix => {
-    return artifact.target_path?.startsWith?.(prefix)
-  })
+  const iter = getIter(artifact)
+  const isTargetPathValid = getIsTargetPathValid(artifact, frontendSpec)
 
   return {
     data: {
