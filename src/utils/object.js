a8a3530af2c0d2180e3fdbe7610ff913a8e6faac
viktor-ilchuk <109525572+viktor-ilchuk@users.noreply.github.com>
2023-05-22 09:42:29 +0300

    Fix [Jobs and Workflows] incorrect boolean chips view (#1729)
    Co-authored-by: Viktor Ilchuk <viktor.ilchuk@amcnetworks.com>

--- src/utils/object.js
+++ src/utils/object.js
@@ -17,13 +17,14 @@ illegal under applicable law, and the grant of the foregoing license
 under the Apache 2.0 license is conditioned upon your compliance with
 such restriction.
 */
+import { isNil } from 'lodash'
 
 // {key: 'value'} --> ["key: value", "key: value"]
 export const parseKeyValues = (object = {}) =>
   object == null
     ? []
     : Object.entries(object).map(([key, value]) => {
-        return Array.isArray(value) && value.every(item => item)
+        return Array.isArray(value) && value.every(item => !isNil(item))
           ? `${key}: [${value.map(arrayItem => {
               return typeof arrayItem === 'object'
                 ? ` {${Object.entries(arrayItem).map(
