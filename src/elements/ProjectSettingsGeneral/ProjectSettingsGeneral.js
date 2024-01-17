1d3eef4502bdacc1d78beb3488cfc6cf973b7c5c
Taras.Hlukhovetskyi <taras.hlukhovetskyi@globallogic.com>
2024-01-17 16:53:28 +0200

    Fix [Redirection] Wrong link redirection with invalid project name

--- src/elements/ProjectSettingsGeneral/ProjectSettingsGeneral.js
+++ src/elements/ProjectSettingsGeneral/ProjectSettingsGeneral.js
@@ -279,13 +279,20 @@ const ProjectSettingsGeneral = ({
   }
 
   useEffect(() => {
-    fetchProject(params.projectName)
+    fetchProject(params.projectName).catch(error => {
+      const customErrorMsg =
+        error.response?.status === FORBIDDEN_ERROR_STATUS_CODE
+          ? 'Permission denied'
+          : getErrorMsg(error, 'Failed to fetch project data')
+
+      setTimeout(() => showErrorNotification(dispatch, error, '', customErrorMsg), 500)
+    })
 
     return () => {
       removeProjectData()
       setEditProjectData(initialEditProjectData)
     }
-  }, [removeProjectData, params.pageTab, params.projectName, fetchProject])
+  }, [removeProjectData, params.pageTab, params.projectName, fetchProject, dispatch])
 
   return (
     <ProjectSettingsGeneralView
