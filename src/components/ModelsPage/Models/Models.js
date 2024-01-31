448ace32cb385f82667463663f13c6aad8a0c3d1
mariana-furyk <mariana.furyk@globallogic.com>
2024-01-31 16:28:36 +0200

    Fix [Models] data displaying issue (reverted to the chips column)

--- src/components/ModelsPage/Models/Models.js
+++ src/components/ModelsPage/Models/Models.js
@@ -75,9 +75,10 @@ const Models = ({ fetchModelFeatureVector }) => {
   const [largeRequestErrorMessage, setLargeRequestErrorMessage] = useState('')
   const [selectedModel, setSelectedModel] = useState({})
   const [selectedRowData, setSelectedRowData] = useState({})
-  const [metricsCounter, setMetricsCounter] = useState(0)
-  const [dataIsLoaded, setDataIsLoaded] = useState(false)
-  const [tableHeaders, setTableHeaders] = useState([])
+  //temporarily commented till ML-5606 will be done
+  // const [metricsCounter, setMetricsCounter] = useState(0)
+  // const [dataIsLoaded, setDataIsLoaded] = useState(false)
+  // const [tableHeaders, setTableHeaders] = useState([])
   const [urlTagOption] = useGetTagOptions(null, filters, null, MODELS_FILTERS)
   const artifactsStore = useSelector(store => store.artifactsStore)
   const detailsStore = useSelector(store => store.detailsStore)
@@ -148,8 +149,9 @@ const Models = ({ fetchModelFeatureVector }) => {
       setSelectedRowData({})
       setModels([])
       setAllModels([])
-      setTableHeaders([])
-      setDataIsLoaded(false)
+      //temporarily commented till ML-5606 will be done
+      // setTableHeaders([])
+      // setDataIsLoaded(false)
 
       return fetchData(filters)
     },
@@ -224,18 +226,12 @@ const Models = ({ fetchModelFeatureVector }) => {
         modelsFilters.iter,
         modelsFilters.tag,
         params.projectName,
-        frontendSpec,
-        metricsCounter
+        frontendSpec
+        //temporarily commented till ML-5606 will be done
+        // metricsCounter
       )
     },
-    [
-      dispatch,
-      modelsFilters.iter,
-      modelsFilters.tag,
-      params.projectName,
-      frontendSpec,
-      metricsCounter
-    ]
+    [dispatch, modelsFilters.iter, modelsFilters.tag, params.projectName, frontendSpec]
   )
 
   const { latestItems, handleExpandRow } = useGroupContent(
@@ -251,18 +247,14 @@ const Models = ({ fetchModelFeatureVector }) => {
   const tableContent = useMemo(() => {
     return filtersStore.groupBy === GROUP_BY_NAME
       ? latestItems.map(contentItem => {
-          return createModelsRowData(
-            contentItem,
-            params.projectName,
-            frontendSpec,
-            metricsCounter,
-            true
-          )
+          return createModelsRowData(contentItem, params.projectName, frontendSpec, null, true)
         })
       : models.map(contentItem =>
-          createModelsRowData(contentItem, params.projectName, frontendSpec, metricsCounter)
+          createModelsRowData(contentItem, params.projectName, frontendSpec)
         )
-  }, [filtersStore.groupBy, frontendSpec, latestItems, metricsCounter, models, params.projectName])
+  }, [filtersStore.groupBy, frontendSpec, latestItems, models, params.projectName])
+
+  const tableHeaders = useMemo(() => tableContent[0]?.content ?? [], [tableContent])
 
   const { sortTable, selectedColumnName, getSortingIcon, sortedTableContent, sortedTableHeaders } =
     useSortTable({
@@ -321,8 +313,9 @@ const Models = ({ fetchModelFeatureVector }) => {
       setAllModels([])
       dispatch(removeModels())
       setSelectedModel({})
-      setTableHeaders([])
-      setDataIsLoaded(false)
+      //temporarily commented till ML-5606 will be done
+      // setTableHeaders([])
+      // setDataIsLoaded(false)
       abortControllerRef.current.abort(REQUEST_CANCELED)
     }
   }, [dispatch, params.projectName, setModels, setAllModels])
@@ -371,38 +364,39 @@ const Models = ({ fetchModelFeatureVector }) => {
     selectedModel.feature_vector
   ])
 
-  useEffect(() => {
-    if (tableContent?.[0]?.content?.length > 0 && tableHeaders.length === 0) {
-      setTableHeaders(tableContent?.[0]?.content)
-    }
-  }, [tableContent, tableHeaders.length])
+  //temporarily commented till ML-5606 will be done
+  // useEffect(() => {
+  //   if (tableContent?.[0]?.content?.length > 0 && tableHeaders.length === 0) {
+  //     setTableHeaders(tableContent?.[0]?.content)
+  //   }
+  // }, [tableContent, tableHeaders.length])
 
-  useEffect(() => {
-    if (models.length > 0 && tableHeaders.length > 0 && !dataIsLoaded) {
-      const newTableHeaders = []
-      const maxMetricsModel = models.reduce((modelWithMaxMetrics, model) => {
-        if (
-          model.metrics &&
-          Object.keys(model.metrics).length > Object.keys(modelWithMaxMetrics.metrics || {}).length
-        ) {
-          return model
-        }
-
-        return modelWithMaxMetrics
-      }, {})
-      Object.keys(maxMetricsModel.metrics ?? {}).forEach(metricKey => {
-        newTableHeaders.push({
-          headerId: metricKey,
-          headerLabel: metricKey,
-          className: 'table-cell-1'
-        })
-      })
-
-      setMetricsCounter(Object.keys(maxMetricsModel.metrics ?? {}).length)
-      setTableHeaders(state => [...state, ...newTableHeaders])
-      setDataIsLoaded(true)
-    }
-  }, [dataIsLoaded, models, tableHeaders])
+  // useEffect(() => {
+  //   if (models.length > 0 && tableHeaders.length > 0 && !dataIsLoaded) {
+  //     const newTableHeaders = []
+  //     const maxMetricsModel = models.reduce((modelWithMaxMetrics, model) => {
+  //       if (
+  //         model.metrics &&
+  //         Object.keys(model.metrics).length > Object.keys(modelWithMaxMetrics.metrics || {}).length
+  //       ) {
+  //         return model
+  //       }
+  //
+  //       return modelWithMaxMetrics
+  //     }, {})
+  //     Object.keys(maxMetricsModel.metrics ?? {}).forEach(metricKey => {
+  //       newTableHeaders.push({
+  //         headerId: metricKey,
+  //         headerLabel: metricKey,
+  //         className: 'table-cell-1'
+  //       })
+  //     })
+  //
+  //     setMetricsCounter(Object.keys(maxMetricsModel.metrics ?? {}).length)
+  //     setTableHeaders(state => [...state, ...newTableHeaders])
+  //     setDataIsLoaded(true)
+  //   }
+  // }, [dataIsLoaded, models, tableHeaders])
 
   const handleRegisterModel = useCallback(() => {
     openPopUp(RegisterModelModal, {
