7a816b0a8ec41b0052c1970473788a99eab6f31b
illia-prokopchuk <78905712+illia-prokopchuk@users.noreply.github.com>
2024-06-05 17:46:41 +0300

    Fix [Functions] new Function button doesn't work (#2506)

--- src/components/FunctionsPage/FunctionsView.js
+++ src/components/FunctionsPage/FunctionsView.js
@@ -99,11 +99,12 @@ const FunctionsView = React.forwardRef(
                   handleRefresh={filtersChangeCallback}
                   actionButtons={[
                     {
-                      className: 'action-button',
                       hidden: !isDemoMode,
-                      label: 'New',
-                      onClick: getPopUpTemplate,
-                      variant: SECONDARY_BUTTON
+                      template: getPopUpTemplate({
+                        className: 'action-button',
+                        label: 'New',
+                        variant: SECONDARY_BUTTON
+                      }),
                     }
                   ]}
                 >
