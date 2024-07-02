a78e95da0d70f9af2e981b61022e6f577fe1a469
orkoresh4 <orkoresh4@gmail.com>
2024-07-02 16:05:28 +0300

    add Jenkinsfile

--- ui_ci.groovy
+++ ui_ci.groovy
@@ -1,45 +1,31 @@
-@Library('pipelinex@development') _
-import com.iguazio.pipelinex.DockerRepo
+@Library('pipelinex@development')
+import com.iguazio.pipelinex.JobException
 
-pipeline {
-    agent { label 'ubuntu_ui_runner' } // Specify the runner here
 
-    environment {
-        REACT_APP_FUNCTION_CATALOG_URL = 'https://raw.githubusercontent.com/mlrun/functions/master'
-        REACT_APP_MLRUN_API_URL = 'http://localhost:30000/mlrun-api-ingress.default-tenant.app.vmdev36.lab.iguazeng.com'
-        REACT_APP_NUCLIO_API_URL = 'http://localhost:30000/nuclio-ingress.default-tenant.app.vmdev36.lab.iguazeng.com'
-        REACT_APP_IGUAZIO_API_URL = 'http://localhost:30000/platform-api.default-tenant.app.vmdev36.lab.iguazeng.com'
-        ARTIFACTORY_URL = 'http://artifactory.example.com/path/to/artifacts/cucumber_report.html'
-        ARTIFACTORY_USER = credentials('artifactory-username') // Assume you have these credentials stored in Jenkins
-        ARTIFACTORY_PASSWORD = credentials('artifactory-password')
-        SLACK_TOKEN = credentials('slack-bot-token')
-        SLACK_CHANNEL = '#your-slack-channel'
-        ARTIFACTORY_BASE_URL = 'http://artifactory.example.com/path/to/artifacts/' // Base URL for Artifactory
-    }
 
-    triggers {
-        cron('H 0 * * *') // Run the job nightly at midnight
-    }
+def node_label = 'ubuntu_ui_runner'
 
-    stages {
-        stage('Pull Latest Changes') {
-            steps {
-                script {
+common.set_current_display_name("UI_Tests")
+
+common.main {
+    timestamps {
+        nodes.runner(node_label) {
+            withCredentials([
+                    usernamePassword(credentialsId: 'iguazio-credentials', usernameVariable: 'RUNNER_USER', passwordVariable: 'RUNNER_PASSWORD')
+            ]) {
+
+                common.conditional_stage('Pull Latest Changes', true) {
                     dir('/root/ui') {
+                        deleteDir()
+                        checkout scm
                         sh 'git pull'
                     }
                 }
-            }
-        }
 
-        stage('Set up Environment') {
-            steps {
-                script {
+                common.conditional_stage('Set up Environment', true) {
                     dir('/root/ui') {
-                        // You can uncomment the following line if npm install is needed
+                        // Uncomment the following line if npm install is needed
                         // sh 'npm install'
-
-                        // Ensure environment variables are set for the session
                         sh '''
                             export REACT_APP_FUNCTION_CATALOG_URL=${REACT_APP_FUNCTION_CATALOG_URL}
                             export REACT_APP_MLRUN_API_URL=${REACT_APP_MLRUN_API_URL}
@@ -48,77 +34,67 @@ pipeline {
                         '''
                     }
                 }
-            }
-        }
 
-        stage('Start Services') {
-            steps {
-                script {
+                common.conditional_stage('Start Services', true) {
                     dir('/root/ui') {
                         // Start mock-server and application in the background
                         sh 'npm run mock-server &'
                         sh 'npm start &'
                     }
                 }
-            }
-        }
 
-        stage('Run Regression Tests') {
-            steps {
-                script {
-                    dir('/root/ui') {
-                        // Run regression tests
-                        sh 'npm run test:regression'
-                    }
-                }
-            }
-        }
+       //        common.conditional_stage('Run Regression Tests', true) {
+       //            dir('/root/ui') {
+       //                // Run cucumber-js tests
+       //                sh './node_modules/.bin/cucumber-js --require-module @babel/register --require-module @babel/polyfill -f json:tests/reports/cucumber_report.json -f html:tests/reports/cucumber_report_default.html tests -t \'@smoke\''
+       //            }
+       //        }
 
-        stage('Post-Test Cleanup') {
-            steps {
-                script {
-                    // Ensure background processes are killed after tests
+                common.conditional_stage('Post-Test Cleanup', true) {
                     sh 'kill %1 || true'
                     sh 'kill %2 || true'
                 }
-            }
-        }
 
-        stage('Upload Artifacts') {
-            steps {
-                script {
+                common.conditional_stage('Upload Artifacts', true) {
                     dir('/root/ui') {
-                        // Upload the test report to Artifactory
                         sh '''
-                            curl -u ${ARTIFACTORY_USER}:${ARTIFACTORY_PASSWORD} -T tests/reports/cucumber_report.html ${ARTIFACTORY_URL}
+                            # Environment variables
+                            ART_URL="http://artifactory.iguazeng.com:8082/artifactory/ui-ci-reports"
+                            AUTH="${ARTIFACTORY_USER}:${ARTIFACTORY_PASSWORD}"
+                            LOCAL_FILE="tests/reports/cucumber_report_default.html"
+
+                            # Generate the Artifactory path with the build number
+                            ARTIFACTORY_PATH="cucumber_report_defualt_${env.BUILD_NUMBER}.html"
+
+                            # Construct the full URL
+                            URL="${ART_URL}/${ARTIFACTORY_PATH}"
+
+                            # Upload the file to Artifactory
+                            curl -X PUT -u ${AUTH} "${URL}" --data-binary @"${LOCAL_FILE}"
                         '''
                     }
                 }
-            }
-        }
 
-        stage('Send Report Link to Slack') {
-            steps {
-                script {
-                    // Construct the URL to the uploaded report
-                    def report_url = "${ARTIFACTORY_BASE_URL}cucumber_report.html"
-
-                    // Send the link to Slack
-                    sh """
-                        curl -X POST -H 'Content-type: application/json' --data '{"channel": "${SLACK_CHANNEL}", "text": "Here is the latest regression test report: ${report_url}"}' \
-                        -H "Authorization: Bearer ${SLACK_TOKEN}" https://slack.com/api/chat.postMessage
-                    """
-                }
+       //        common.conditional_stage('Send Report Link to Slack', true) {
+       //            script {
+       //                def report_url = "http://artifactory.iguazeng.com:8082/artifactory/ui-ci-reports/cucumber_report_defualt_${env.BUILD_NUMBER}.html"
+       //                // Send the link to Slack
+       //                sh """
+       //                    curl -X POST -H 'Content-type: application/json' --data '{"channel": "${SLACK_CHANNEL}", "text": "Here is the latest regression test report: ${report_url}"}' \
+       //                    -H "Authorization: Bearer ${SLACK_TOKEN}" https://slack.com/api/chat.postMessage
+       //                """
+       //            }
+       //        }
             }
         }
     }
+}
 
-    post {
-        always {
-            script {
-                // Ensure any remaining background processes are terminated
-                sh 'pkill -f npm || true'
-            }
+post {
+    always {
+        script {
+            // Ensure any remaining background processes are terminated
+            sh 'pkill -f npm || true'
         }
     }
 }
