ce3998fbd9b938e2af30568fcb86c220a749cce5
pinis-gini-apps <pinis@gini-apps.com>
2024-09-05 10:58:46 +0300

    Fix: Prevent modal from closing when clicking on tooltips or overlay containe

--- src/elements/MetricsSelector/MetricsSelector.js
+++ src/elements/MetricsSelector/MetricsSelector.js
@@ -113,6 +113,11 @@ const MetricsSelector = ({
 
   const windowClickHandler = useCallback(
     event => {
+      const isTooltip = event.target.closest('.tooltip-template')
+      const isOverlayContainer = event.target.closest('#overlay_container')
+
+      if (isTooltip || isOverlayContainer) return
+
       if (
         !event.target.closest('.metrics-selector-popup') &&
         !event.target.closest('.metrics-selector')
