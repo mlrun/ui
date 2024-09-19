1b509a20e59db98c7c527dc3a2c3e4cb36eb7b46
illia-prokopchuk <78905712+illia-prokopchuk@users.noreply.github.com>
2024-09-19 15:34:54 +0300

    Impl [Artifacts] Add mock for GET `artifacts` request (#2742)

--- tests/mockServer/mock.js
+++ tests/mockServer/mock.js
@@ -1227,6 +1227,22 @@ function getArtifacts(req, res) {
     }
   }
 
+  if (req.query['format'] === 'minimal') {
+    collectedArtifacts = collectedArtifacts.map(func => {
+      const fieldsToPick = [
+        'db_key',
+        'producer',
+        'size',
+        'target_path',
+        'framework',
+        'metrics'
+      ]
+      const specFieldsToPick = fieldsToPick.map(fieldName => `spec.${fieldName}`)
+
+      return pick(func, ['kind', 'metadata', 'status', 'project', ...specFieldsToPick, ...fieldsToPick])
+    })
+  }
+
   res.send({ artifacts: collectedArtifacts })
 }
 
