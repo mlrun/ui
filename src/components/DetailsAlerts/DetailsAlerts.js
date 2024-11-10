04db5c3719fc3894c13e147d7b05e1d514f67ca6
adi-gini <adiu@gini-apps.com>
2024-11-10 15:19:36 +0200

    add: alerts page

---
+++ src/components/DetailsAlerts/DetailsAlerts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DetailsAlerts = ({ selectedItem }) => {
+  return (
+    <div>
+      <h2>Alerts</h2>
+      <p>This tab shows alerts for the selected item.</p>
+      {/* Add more content here as needed */}
+    </div>
+  )
+}
+
+export default DetailsAlerts
