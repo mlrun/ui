cc90fc8a1fc4e227d82c3f0ba6f358db553a5017
adi-gini <adiu@gini-apps.com>
2024-12-02 15:04:48 +0200

    fix

--- src/elements/ProjectsMonitoringCounters/AlertsCounters.js
+++ src/elements/ProjectsMonitoringCounters/AlertsCounters.js
@@ -47,7 +47,7 @@ const AlertsCounters = () => {
     }
 
     if (path !== '*') {
-      const endpoint = projectStore.projectSummary.data.endpoint_alerts_count || 1
+      const endpoint = projectStore.projectSummary.data.endpoint_alerts_count || 0
       const jobs = projectStore.projectSummary.data.job_alerts_count || 0
       const application = projectStore.projectSummary.data.other_alerts_count || 0
 
