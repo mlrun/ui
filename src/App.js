e8f3d55a81e7e3239c9722bf2bfbffe0f93a9d71
mariana-furyk <mariana.furyk@globallogic.com>
2024-12-12 19:54:23 +0200

    Impl [VectorDB] Add Overview details tab

--- src/App.js
+++ src/App.js
@@ -213,7 +213,7 @@ const App = () => {
             `projects/:projectName/functions/:funcName/${ALL_VERSIONS_PATH}/:id/:tab`
           ].map((path, index) => (
             <Fragment key={index}>
-              <Route path={path} element={<Functions isAllVersions={[2,3].includes(index)} />} />
+              <Route path={path} element={<Functions isAllVersions={[2, 3].includes(index)} />} />
             </Fragment>
           ))}
           <Route
@@ -290,7 +290,11 @@ const App = () => {
               <Route path={path} element={<Files />} />
             </Fragment>
           ))}
-          {['projects/:projectName/documents'].map((path, index) => (
+          {[
+            'projects/:projectName/documents',
+            'projects/:projectName/documents/:name/:tag/:tab',
+            'projects/:projectName/documents/:name/:tag/:iter/:tab'
+          ].map((path, index) => (
             <Fragment key={index}>
               <Route path={path} element={<Documents />} />
             </Fragment>
