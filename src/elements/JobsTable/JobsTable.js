f25f88e98c82285fb5ec9e70e6452f3aeca4a9ae
ilan7empest <therecanbeonly1.1984@gmail.com>
2024-12-13 12:51:02 +0200

    remove unused props

--- src/elements/JobsTable/JobsTable.js
+++ src/elements/JobsTable/JobsTable.js
@@ -59,9 +59,6 @@ const JobsTable = React.forwardRef(
       context,
       filters,
       filtersConfig,
-      jobRuns,
-      jobs,
-      navigateLink,
       paginatedJobs,
       refreshJobs,
       requestErrorMessage,
@@ -71,8 +68,7 @@ const JobsTable = React.forwardRef(
       setJobRuns,
       setJobs,
       setSelectedJob,
-      tableContent,
-      terminateAbortTasksPolling
+      tableContent
     },
     { abortJobRef, fetchJobFunctionsPromiseRef }
   ) => {
