5d707e2a3da5b76ce1afc45dfbb8d1105e4cc483
ilan7empest <therecanbeonly1.1984@gmail.com>
2024-12-21 23:22:54 +0200

    Fix [Documents] Wrong 'Collection' view

--- src/components/Documents/documents.util.js
+++ src/components/Documents/documents.util.js
@@ -33,6 +33,7 @@ import {
   NAME_FILTER,
   TAG_FILTER,
   TAG_FILTER_ALL_ITEMS,
+  TAG_FILTER_LATEST,
   VIEW_SEARCH_PARAMETER
 } from '../../constants'
 import { getIsTargetPathValid } from '../../utils/createArtifactsContent'
@@ -60,8 +61,12 @@ import { ReactComponent as DownloadIcon } from 'igz-controls/images/download.svg
 import { ReactComponent as HistoryIcon } from 'igz-controls/images/history.svg'
 
 export const getFiltersConfig = isAllVersions => ({
-  [NAME_FILTER]: { label: 'Name:', initialValue: '' },
-  [TAG_FILTER]: { label: 'Version tag:', initialValue: TAG_FILTER_ALL_ITEMS, isModal: true },
+  [NAME_FILTER]: { label: 'Name:', initialValue: '', hidden: isAllVersions },
+  [TAG_FILTER]: {
+    label: 'Version tag:',
+    initialValue: isAllVersions ? TAG_FILTER_ALL_ITEMS : TAG_FILTER_LATEST,
+    isModal: true
+  },
   [LABELS_FILTER]: { label: 'Labels:', initialValue: '', isModal: true },
   [ITERATIONS_FILTER]: {
     label: 'Show best iteration only:',
