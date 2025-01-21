fefa63fe3cfb285e630e3accf5515728ac406813
adi-gini <adiu@gini-apps.com>
2025-01-21 18:57:29 +0200

    Add: default sorting by ‘updated’ to feature set

--- src/components/FeatureStore/FeatureSets/FeatureSets.js
+++ src/components/FeatureStore/FeatureSets/FeatureSets.js
@@ -52,6 +52,7 @@ import { isDetailsTabExists } from '../../../utils/link-helper.util'
 import { parseChipsData } from '../../../utils/convertChipsData'
 import { parseFeatureSets } from '../../../utils/parseFeatureSets'
 import { setNotification } from '../../../reducers/notificationReducer'
+import { sortListByDate } from '../../../utils'
 import { useGroupContent } from '../../../hooks/groupContent.hook'
 import { useOpenPanel } from '../../../hooks/openPanel.hook'
 import { useVirtualization } from '../../../hooks/useVirtualization.hook'
@@ -235,10 +236,10 @@ const FeatureSets = ({
 
   const tableContent = useMemo(() => {
     return filtersStore.groupBy === GROUP_BY_NAME
-      ? latestItems.map(contentItem => {
+      ? sortListByDate(latestItems, 'updated', false).map(contentItem => {
           return createFeatureSetsRowData(contentItem, params.projectName, FEATURE_SETS_TAB, true)
         })
-      : featureSets.map(contentItem =>
+      : sortListByDate(featureSets, 'updated', false).map(contentItem =>
           createFeatureSetsRowData(contentItem, params.projectName, FEATURE_SETS_TAB)
         )
   }, [featureSets, filtersStore.groupBy, latestItems, params.projectName])
