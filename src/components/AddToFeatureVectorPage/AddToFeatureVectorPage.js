8d9cf91770fb4dcdcb45fe9dc38f06096d028f4b
Taras-Hlukhovetskyi <155433425+Taras-Hlukhovetskyi@users.noreply.github.com>
2025-01-28 15:37:02 +0200

    Fix [Create vector] tags request should be send when changing project  (#3059)

--- src/components/AddToFeatureVectorPage/AddToFeatureVectorPage.js
+++ src/components/AddToFeatureVectorPage/AddToFeatureVectorPage.js
@@ -357,6 +357,7 @@ const AddToFeatureVectorPage = ({
       actionsMenu={actionsMenu}
       content={content}
       featureStore={featureStore}
+      fetchTags={fetchTags}
       filters={addToFeatureVectorFilters}
       filtersConfig={filtersConfig}
       filtersStore={filtersStore}
