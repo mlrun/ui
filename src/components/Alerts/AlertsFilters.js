309d3d08ec717ff0f42fdcc781043be257361e89
Ilank <63646693+ilan7empest@users.noreply.github.com>
2025-02-17 12:48:18 +0200

    Fix [Alerts] The Event type filter doesn't align with the Entity type option (#3099)

--- src/components/Alerts/AlertsFilters.js
+++ src/components/Alerts/AlertsFilters.js
@@ -95,7 +95,10 @@ const AlertsFilters = ({ isAlertsPage, isCrossProjects }) => {
 
     form.change(
       EVENT_TYPE,
-      params[ENTITY_TYPE] === selectedValue || entityType === 'all' ? params[EVENT_TYPE] : 'all'
+      params[ENTITY_TYPE] === selectedValue ||
+        (entityType === FILTER_ALL_ITEMS && params[EVENT_TYPE])
+        ? params[EVENT_TYPE]
+        : FILTER_ALL_ITEMS
     )
   }
 
