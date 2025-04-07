486dfe127f4e80b0d2da3bf130cff26e5782c808
Taras-Hlukhovetskyi <155433425+Taras-Hlukhovetskyi@users.noreply.github.com>
2025-04-07 21:01:38 +0300

    Fix [Project settings] The Delete project  button is not clickable (#3204)

--- src/components/ProjectsPage/projects.util.js
+++ src/components/ProjectsPage/projects.util.js
@@ -145,7 +145,7 @@ export const projectsSortOptions = [
 export const handleDeleteProjectError = (
   error,
   handleDeleteProject,
-  project,
+  projectName,
   setConfirmData,
   dispatch,
   deleteNonEmpty,
@@ -157,10 +157,10 @@ export const handleDeleteProjectError = (
 ) => {
   if (error.response?.status === 412 && !deleteNonEmpty) {
     setConfirmData({
-      item: project,
+      item: projectName,
       header: 'Delete project?',
       message:
-        `You are trying to delete the non-empty project "${project.metadata.name}". Deleting it will also delete all of its resources, such as jobs, ` +
+        `You are trying to delete the non-empty project "${projectName}". Deleting it will also delete all of its resources, such as jobs, ` +
         'artifacts, and features.',
       btnConfirmLabel: 'Delete',
       btnConfirmType: DANGER_BUTTON,
@@ -169,7 +169,7 @@ export const handleDeleteProjectError = (
       },
       confirmHandler: () => {
         handleDeleteProject(
-          project,
+          projectName,
           true,
           setConfirmData,
           dispatch,
@@ -184,12 +184,12 @@ export const handleDeleteProjectError = (
   } else {
     const customErrorMsg =
       error.response?.status === FORBIDDEN_ERROR_STATUS_CODE
-        ? `You do not have permission to delete the project ${project.metadata.name} `
-        : `Failed to delete the project ${project.metadata.name}`
+        ? `You do not have permission to delete the project ${projectName} `
+        : `Failed to delete the project ${projectName}`
 
     showErrorNotification(dispatch, error, '', customErrorMsg, () =>
       handleDeleteProject(
-        project,
+        projectName,
         false,
         setConfirmData,
         dispatch,
@@ -330,22 +330,22 @@ export const generateMonitoringCounters = (data, dispatch) => {
   dispatch(setJobsMonitoringData(monitoringCounters))
 }
 
-export const onDeleteProject = (project, setConfirmData, ...args) => {
+export const onDeleteProject = (projectName, setConfirmData, ...args) => {
   setConfirmData({
-    item: project,
+    item: projectName,
     header: 'Delete project?',
-    message: `You are trying to delete the project "${project.metadata.name}". Deleted projects cannot be restored`,
+    message: `You are trying to delete the project "${projectName}". Deleted projects cannot be restored`,
     btnConfirmLabel: 'Delete',
     btnConfirmType: DANGER_BUTTON,
     rejectHandler: () => {
       setConfirmData(null)
     },
-    confirmHandler: () => handleDeleteProject(project, false, setConfirmData, ...args)
+    confirmHandler: () => handleDeleteProject(projectName, false, setConfirmData, ...args)
   })
 }
 
 export const handleDeleteProject = (
-  project,
+  projectName,
   deleteNonEmpty,
   setConfirmData,
   dispatch,
@@ -357,7 +357,7 @@ export const handleDeleteProject = (
 ) => {
   setConfirmData && setConfirmData(null)
 
-  dispatch(deleteProject({ projectName: project.metadata.name, deleteNonEmpty }))
+  dispatch(deleteProject({ projectName, deleteNonEmpty }))
     .unwrap()
     .then(({ response }) => {
       if (isBackgroundTaskRunning(response)) {
@@ -389,7 +389,7 @@ export const handleDeleteProject = (
           setNotification({
             status: 200,
             id: Math.random(),
-            message: `Project "${project.metadata.name}" was deleted successfully`
+            message: `Project "${projectName}" was deleted successfully`
           })
         )
         if (navigate) {
@@ -401,7 +401,7 @@ export const handleDeleteProject = (
       handleDeleteProjectError(
         error,
         handleDeleteProject,
-        project,
+        projectName,
         setConfirmData,
         dispatch,
         deleteNonEmpty,
