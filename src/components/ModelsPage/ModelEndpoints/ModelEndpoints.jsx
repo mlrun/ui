4a1310d2fe975b7dc67b6164b99b63d6cd82253b
ilan7empest <therecanbeonly1.1984@gmail.com>
2025-07-30 10:03:28 +0300

    Impl [Navbar] new look by Figma

--- src/components/ModelsPage/ModelEndpoints/ModelEndpoints.jsx
+++ src/components/ModelsPage/ModelEndpoints/ModelEndpoints.jsx
@@ -25,6 +25,7 @@ import { isEmpty } from 'lodash'
 import ActionBar from '../../ActionBar/ActionBar'
 import ArtifactsTableRow from '../../../elements/ArtifactsTableRow/ArtifactsTableRow'
 import ModelEndpointsFilters from './ModelEndpointsFilters'
+import ModelsPageTabs from '../ModelsPageTabs/ModelsPageTabs'
 import NoData from '../../../common/NoData/NoData'
 import Table from '../../Table/Table'
 import { Loader } from 'igz-controls/components'
@@ -267,6 +268,7 @@ const ModelEndpoints = () => {
       <div className="models" ref={modelEndpointsRef}>
         <div className="table-container">
           <div className="content__action-bar-wrapper">
+            <ModelsPageTabs />
             <ActionBar
               filters={filters}
               filtersConfig={filtersConfig}
