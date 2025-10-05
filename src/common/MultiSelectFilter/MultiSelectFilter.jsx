730df23a21c75bda09f66bc649d1a7226eddd995
ilan7empest <therecanbeonly1.1984@gmail.com>
2025-10-05 20:09:10 +0300

    Fix [UI] Unexpected 'jobs' presentation for scheduled jobs  link

---
+++ src/common/MultiSelectFilter/MultiSelectFilter.jsx
@@ -0,0 +1,75 @@
+/*
+Copyright 2019 Iguazio Systems Ltd.
+Licensed under the Apache License, Version 2.0 (the "License") with
+an addition restriction as set forth herein. You may not use this
+file except in compliance with the License. You may obtain a copy of
+the License at http://www.apache.org/licenses/LICENSE-2.0.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the License for the specific language governing
+permissions and limitations under the License.
+In addition, you may not use the software for any purposes that are
+illegal under applicable law, and the grant of the foregoing license
+under the Apache 2.0 license is conditioned upon your compliance with
+such restriction.
+*/
+import React, { useMemo, memo } from 'react'
+import PropTypes from 'prop-types'
+import { useField, useForm } from 'react-final-form'
+import { isEmpty, capitalize } from 'lodash'
+
+import { FormOnChange, FormSelect } from 'igz-controls/components'
+import { FILTER_ALL_ITEMS } from '../../constants'
+import { OPTIONS_LIST } from '../../types'
+
+// TODO: Rename component from MultiSelectFilter to a more generic name
+const MultiSelectFilter = ({ optionsList, name }) => {
+  const { input } = useField(name)
+  const { change } = useForm()
+
+  const mappedoptionsList = useMemo(() => {
+    const isAllOptionsSelected = input.value?.includes?.(FILTER_ALL_ITEMS)
+    return isAllOptionsSelected
+      ? optionsList.map(option =>
+        option.id === FILTER_ALL_ITEMS ? { ...option, disabled: true } : option
+      )
+      : optionsList
+  }, [input.value, optionsList])
+
+  const handleFilterOptionChange = (selectedValue, currentValue) => {
+    if (
+      selectedValue.length > 1 &&
+      selectedValue.includes(FILTER_ALL_ITEMS) &&
+      selectedValue.indexOf(FILTER_ALL_ITEMS) === 0
+    ) {
+      change(
+        name,
+        selectedValue.filter(value => value !== FILTER_ALL_ITEMS)
+      )
+    } else if (
+      isEmpty(selectedValue) ||
+      (!currentValue.includes(FILTER_ALL_ITEMS) &&
+        selectedValue.includes(FILTER_ALL_ITEMS) &&
+        selectedValue.indexOf(FILTER_ALL_ITEMS) > 0) ||
+      mappedoptionsList.filter(option => option.id !== FILTER_ALL_ITEMS).length ===
+      selectedValue.length
+    ) {
+      change(name, [FILTER_ALL_ITEMS])
+    }
+  }
+
+  return (
+    <>
+      <FormSelect label={capitalize(name)} name={name} options={mappedoptionsList} multiple />
+      <FormOnChange handler={(value, some) => handleFilterOptionChange(value, some)} name={name} />
+    </>
+  )
+}
+
+MultiSelectFilter.propTypes = {
+  name: PropTypes.string.isRequired,
+  optionsList: OPTIONS_LIST.isRequired
+}
+
+export default memo(MultiSelectFilter)
