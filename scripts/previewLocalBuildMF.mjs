dffc5112c7434a6c9b56994b4b3633a119ffdcfd
pinis-gini-apps <pinis@gini-apps.com>
2025-10-23 10:25:18 +0300

    fix pr comments: rename script to build:federation, change env file to .env.development.mf, minor code format

--- scripts/previewLocalBuildMF.mjs
+++ scripts/previewLocalBuildMF.mjs
@@ -2,7 +2,7 @@ import { spawn } from 'child_process'
 
 const preview = spawn(
   'dotenv',
-  ['-e', '.env.development.local-build', '--', 'vite', 'preview', '--port', '5179'],
+  ['-e', '.env.development.mf', '--', 'vite', 'preview', '--port', '5179'],
   { stdio: 'inherit', shell: true }
 )
 
