2e2be01af83540b258ec898754a1ace7a4284eab
pinis-gini-apps <pinis@gini-apps.com>
2025-10-23 10:25:18 +0300

    fix pr comments: rename script to build:federation, change env file to .env.development.mf, minor code format

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,6 @@ import commonjs from 'vite-plugin-commonjs'
 import eslint from 'vite-plugin-eslint'
 import { federation } from '@module-federation/vite'
 import path from 'path'
-
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 import { defineConfig, loadEnv } from 'vite'
