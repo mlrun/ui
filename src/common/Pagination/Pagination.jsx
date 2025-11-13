68d438768ac1ad83ec29ed98f78399e83bf531df
Andrew Mavdryk <andrii.mavdryk@globallogic.com>
2025-11-13 12:37:02 +0200

    Fix [Pagination] Scroll to table top on pagination (#3495)

--- src/common/Pagination/Pagination.jsx
+++ src/common/Pagination/Pagination.jsx
@@ -17,7 +17,7 @@ illegal under applicable law, and the grant of the foregoing license
 under the Apache 2.0 license is conditioned upon your compliance with
 such restriction.
 */
-import React, { useCallback, useMemo, useRef } from 'react'
+import React, { useCallback, useEffect, useMemo, useRef } from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import { useNavigate, useSearchParams } from 'react-router-dom'
@@ -35,6 +35,7 @@ import {
   ITEMS_COUNT_END,
   ITEMS_COUNT_START
 } from '../../constants'
+import { MAIN_TABLE_ID } from 'igz-controls/constants'
 import { PAGINATION_CONFIG } from '../../types'
 import { getCloseDetailsLink } from '../../utils/link-helper.util'
 
@@ -51,8 +52,9 @@ const Pagination = ({
   disabledNextDoubleBtnTooltip = '',
   paginationConfig,
   selectedItemName = '',
+  tableId = MAIN_TABLE_ID
 }) => {
-  const [, setSearchParams] = useSearchParams()
+  const [searchParams, setSearchParams] = useSearchParams()
   const navigate = useNavigate()
   const paginationPagesRef = useRef()
   const leftSideRef = useRef(0)
@@ -220,6 +222,18 @@ const Pagination = ({
     [paginationConfig]
   )
 
+  useEffect(() => {
+    if (parseInt(searchParams.get(FE_PAGE)) !== paginationConfig[FE_PAGE]) {
+      const tableElement = document.getElementById(tableId)
+
+      if (tableElement) {
+        tableElement.scrollTo({
+          top: 0
+        })
+      }
+    }
+  }, [paginationConfig, searchParams, tableId])
+
   return (
     <div className="pagination">
       {paginationConfig.isNewResponse && (
@@ -321,7 +335,8 @@ Pagination.propTypes = {
   disableNextDoubleBtn: PropTypes.bool,
   disabledNextDoubleBtnTooltip: PropTypes.string,
   paginationConfig: PAGINATION_CONFIG.isRequired,
-  selectedItemName: PropTypes.string
+  selectedItemName: PropTypes.string,
+  tableId: PropTypes.string
 }
 
 export default Pagination
