f69bde640f5e84d4cfb1aec4fff2481aea458617
illia.prokopchuk <illia.prokopchuk@globallogic.com>
2025-11-20 12:43:25 +0200

    [FEAT] Start implementing tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -17,12 +17,4 @@ illegal under applicable law, and the grant of the foregoing license
 under the Apache 2.0 license is conditioned upon your compliance with
 such restriction.
 */
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-
-it('renders without crashing', () => {
-  const root = createRoot(document.getElementById('root'))
-  root.render(<App />)
-  root.unmount()
-})
+import '@testing-library/jest-dom'
--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,12 +1,24 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
     include: ['src/**/*.{test,spec}.{js,jsx}'],
     exclude: ['node_modules', 'build', 'dist'],
+    server: {
+      deps: {
+        inline: ['iguazio.dashboard-react-controls']
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      'igz-controls': path.resolve(__dirname, './node_modules/iguazio.dashboard-react-controls/dist')
+    }
   }
 })
