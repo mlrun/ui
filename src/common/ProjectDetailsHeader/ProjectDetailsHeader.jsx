3c23612306d88605c0d7dc63cb4ef54cf8261e63
Pini Shahmurov1 <pini-sh>
2026-03-17 16:15:44 +0200

    merge with development

--- src/common/ProjectDetailsHeader/ProjectDetailsHeader.jsx
+++ src/common/ProjectDetailsHeader/ProjectDetailsHeader.jsx
@@ -38,7 +38,7 @@ const ProjectDetailsHeader = ({ projectData, projectName }) => {
       {projectData && (
         <div>
           <span className="project-details__details-label">
-            Created: {formatDatetime(projectData.metadata.created + 'Z')}
+            Created: {formatDatetime(projectData.metadata.created.replace('+00:00', '') + 'Z')}
           </span>
           {projectData.spec.owner && !frontendSpec.ce?.version && (
             <span className="project-details__details-label">Owner: {projectData.spec.owner}</span>
--- src/components/DetailsLLMPrompts/detailsLLMPrompts.util.jsx
+++ src/components/DetailsLLMPrompts/detailsLLMPrompts.util.jsx
@@ -26,12 +26,7 @@ import { openPopUp } from 'igz-controls/utils/common.util'
 import { parseChipsData } from '../../utils/convertChipsData'
 import { formatDatetime } from 'igz-controls/utils/datetime.util'
 import JobPopUp from '../../elements/DetailsPopUp/JobPopUp/JobPopUp'
-import {
-  FORCE_REFRESH,
-  LLM_PROMPTS_PAGE,
-  MODEL_NAME_FILTER,
-  MODEL_TAG_FILTER
-} from '../../constants'
+import { FORCE_REFRESH, LLM_PROMPTS_PAGE, MODEL_NAME_FILTER } from '../../constants'
 
 export const LLM_PROMPTS_DISPLAY_LIMIT = 100
 
@@ -132,7 +127,6 @@ export const generateLLMPromptsTabContent = (artifacts, params, isDetailsPopUp =
 export const navigateToLLMPromptsPage = (navigate, projectName, selectedItem, handleCancel) => {
   const filters = {
     [MODEL_NAME_FILTER]: selectedItem.db_key || selectedItem.key,
-    [MODEL_TAG_FILTER]: selectedItem.tag,
     [FORCE_REFRESH]: true
   }
 
