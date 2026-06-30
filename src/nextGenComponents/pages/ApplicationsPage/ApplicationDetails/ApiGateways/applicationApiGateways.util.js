0f93277fcb2691b3d3097d1f07c139d0f337750a
Amit_Noyberg1 <219515702+amit-noy@users.noreply.github.com>
2026-06-30 13:24:21 +0300

    fix: add missing owner domain in Applications -> API Gateways

--- src/nextGenComponents/pages/ApplicationsPage/ApplicationDetails/ApiGateways/applicationApiGateways.util.js
+++ src/nextGenComponents/pages/ApplicationsPage/ApplicationDetails/ApiGateways/applicationApiGateways.util.js
@@ -17,7 +17,7 @@ illegal under applicable law, and the grant of the foregoing license
 under the Apache 2.0 license is conditioned upon your compliance with
 such restriction.
 */
-import { NUCLIO_OWNER_LABEL } from '../applicationDetails.constants'
+import { buildNuclioOwner } from '../../../../../utils/nuclioEnrichment.util'
 
 export {
   buildGatewayEndpoint,
@@ -37,10 +37,7 @@ export const filterApiGatewaysBySearchFields = (gateways, filters) => {
       return false
     }
 
-    if (
-      ownerFilter &&
-      !gateway.metadata?.labels?.[NUCLIO_OWNER_LABEL]?.toLowerCase().includes(ownerFilter)
-    ) {
+    if (ownerFilter && !buildNuclioOwner(gateway.metadata?.labels).toLowerCase().includes(ownerFilter)) {
       return false
     }
 
