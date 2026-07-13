fd75a318de4a9957dc03ec375181a091c346463f
Amit_Noyberg1 <219515702+amit-noy@users.noreply.github.com>
2026-07-13 11:46:40 +0300

    fix: prevent Nuclio router from interfering with host navigation away from Nuclio routes

--- src/components/RemoteNuclio/RemoteNuclioRouteWrapper.jsx
+++ src/components/RemoteNuclio/RemoteNuclioRouteWrapper.jsx
@@ -28,6 +28,9 @@ import Breadcrumbs from '../../common/Breadcrumbs/Breadcrumbs'
 
 const RemoteNuclioApp = React.lazy(() => loadNuclioApp())
 
+const isNuclioPath = pathname =>
+  /^\/projects\/[^/]+\/(real-time-functions|create-function|api-gateways)(\/|$)/.test(pathname)
+
 const RemoteNuclioRouteWrapper = () => {
   const [ready, setReady] = useState(false)
   const [error, setError] = useState(false)
@@ -39,14 +42,18 @@ const RemoteNuclioRouteWrapper = () => {
     history.pushState = function (...args) {
       origPushState.apply(this, args)
       Promise.resolve().then(() => {
-        window.dispatchEvent(new PopStateEvent('popstate'))
+        if (isNuclioPath(window.location.pathname)) {
+          window.dispatchEvent(new PopStateEvent('popstate'))
+        }
       })
     }
 
     history.replaceState = function (...args) {
       origReplaceState.apply(this, args)
       Promise.resolve().then(() => {
-        window.dispatchEvent(new PopStateEvent('popstate'))
+        if (isNuclioPath(window.location.pathname)) {
+          window.dispatchEvent(new PopStateEvent('popstate'))
+        }
       })
     }
 
